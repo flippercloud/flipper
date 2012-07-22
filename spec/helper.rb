d8e014b2a2a9ed2d6c7077846963a7fd2864ecbc
John Nunemaker <nunemaker@gmail.com>
2012-07-22 15:50:39 -0400

    Spec helper file.

---
+++ spec/helper.rb
@@ -0,0 +1,21 @@
+$:.unshift(File.expand_path('../../lib', __FILE__))
+
+require 'pathname'
+require 'logger'
+
+root_path = Pathname(__FILE__).dirname.join('..').expand_path
+lib_path  = root_path.join('lib')
+log_path  = root_path.join('log')
+log_path.mkpath
+
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:default, :test)
+
+require 'flipper'
+
+Logger.new(log_path.join('test.log'))
+
+RSpec.configure do |c|
+end
