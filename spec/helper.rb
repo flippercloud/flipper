1d13c1c90ae133b9e75e49bbfaad849b922be912
John Nunemaker <nunemaker@gmail.com>
2012-07-25 14:49:49 -0400

    Added some rspec goodness

--- spec/helper.rb
+++ spec/helper.rb
@@ -17,6 +17,9 @@ require 'flipper'
 
 Logger.new(log_path.join('test.log'))
 
-RSpec.configure do |c|
-
+RSpec.configure do |config|
+  config.filter_run :focused => true
+  config.alias_example_to :fit, :focused => true
+  config.alias_example_to :xit, :disabled => true
+  config.run_all_when_everything_filtered = true
 end
