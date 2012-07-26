4576e4bd4036cd71540489db673791efef8102e6
John Nunemaker <nunemaker@gmail.com>
2012-07-25 23:42:35 -0400

    More tests around configuration

--- spec/flipper_spec.rb
+++ spec/flipper_spec.rb
@@ -1,21 +1,25 @@
 require 'helper'
 
 describe Flipper do
-  describe ".configuration" do
-    it "defaults to an instance of configuration" do
-      Flipper.configuration.should be_instance_of(Flipper::Configuration)
-    end
+  before do
+    @original_adapter = Flipper.configuration.adapter
+  end
+
+  after do
+    Flipper.configuration.adapter = @original_adapter
+  end
+
+  it "defaults configuration to new instance" do
+    Flipper.configuration.should be_instance_of(Flipper::Configuration)
+  end
+
+  it "has configuration accessor" do
+    new_configuration = Flipper::Configuration.new
+    Flipper.configuration = new_configuration
+    Flipper.configuration.should eq(new_configuration)
   end
 
   describe ".configure" do
-    before do
-      @original_adapter = Flipper.configuration.adapter
-    end
-
-    after do
-      Flipper.configuration.adapter = @original_adapter
-    end
-
     it "yields configuration to block" do
       adapter = double('Adapter')
       Flipper.configure do |config|
