a5465c340b57da6dccb4d309dd8b4d47d5da9675
John Nunemaker <nunemaker@gmail.com>
2012-07-25 23:38:24 -0400

    Use new configuration object in Flipper module

--- lib/flipper.rb
+++ lib/flipper.rb
@@ -1,4 +1,20 @@
+require 'flipper/configuration'
 require 'flipper/feature'
 
 module Flipper
+  module_function
+
+  def configuration
+    @configuration ||= Configuration.new
+  end
+
+  def configuration=(configuration)
+    @configuration = configuration
+  end
+
+  def configure(&block)
+    block.call(configuration)
+  end
+end
+
 end
--- spec/flipper_spec.rb
+++ spec/flipper_spec.rb
@@ -1,4 +1,27 @@
 require 'helper'
 
 describe Flipper do
+  describe ".configuration" do
+    it "defaults to an instance of configuration" do
+      Flipper.configuration.should be_instance_of(Flipper::Configuration)
+    end
+  end
+
+  describe ".configure" do
+    before do
+      @original_adapter = Flipper.configuration.adapter
+    end
+
+    after do
+      Flipper.configuration.adapter = @original_adapter
+    end
+
+    it "yields configuration to block" do
+      adapter = double('Adapter')
+      Flipper.configure do |config|
+        config.adapter = adapter
+      end
+      Flipper.configuration.adapter.should eq(adapter)
+    end
+  end
 end
