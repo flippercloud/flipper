272d6f4efe83044f2903891ff326a291680598aa
John Nunemaker <nunemaker@gmail.com>
2012-07-31 13:11:23 -0400

    Added group example

---
+++ examples/group.rb
@@ -0,0 +1,35 @@
+require './example_setup'
+
+require 'flipper'
+require 'flipper/adapters/memory'
+
+adapter = Flipper::Adapters::Memory.new
+stats = Flipper::Feature.new(:stats, adapter)
+
+# Define group
+admins = Flipper::Types::Group.define(:admins) do |actor|
+  actor.respond_to?(:admin?) && actor.admin?
+end
+
+# Some class that represents what will be trying to do something
+class User
+  def initialize(admin)
+    @admin = admin
+  end
+
+  def admin?
+    @admin == true
+  end
+end
+
+admin = User.new(true)
+non_admin = User.new(false)
+
+puts "Stats for admin: #{stats.enabled?(admin)}"
+puts "Stats for non_admin: #{stats.enabled?(non_admin)}"
+
+puts "\nEnabling Stats for admins...\n\n"
+stats.enable(admins)
+
+puts "Stats for admin: #{stats.enabled?(admin)}"
+puts "Stats for non_admin: #{stats.enabled?(non_admin)}"
