620f045dbcfaa24f4b47e615390ad11231543694
John Nunemaker <nunemaker@gmail.com>
2012-08-02 15:00:07 -0400

    Make xit mark things pending

--- spec/helper.rb
+++ spec/helper.rb
@@ -20,7 +20,7 @@ Logger.new(log_path.join('test.log'))
 RSpec.configure do |config|
   config.filter_run :focused => true
   config.alias_example_to :fit, :focused => true
-  config.alias_example_to :xit, :disabled => true
+  config.alias_example_to :xit, :pending => true
   config.run_all_when_everything_filtered = true
 
   config.before(:each) do
