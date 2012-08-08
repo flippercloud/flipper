c5d598d97ec1c358203d40625ef17e356ba39b55
John Nunemaker <nunemaker@gmail.com>
2012-08-08 10:48:40 -0400

    Make actor handle more
    It now can initialize with most things and delegates to those things
    for missing methods.

--- lib/flipper/types/actor.rb
+++ lib/flipper/types/actor.rb
@@ -1,10 +1,34 @@
 module Flipper
   module Types
     class Actor < Type
+      def self.wrappable?(thing)
+        thing.is_a?(Flipper::Types::Actor) ||
+          thing.respond_to?(:identifier) ||
+          thing.respond_to?(:id) ||
+          thing.respond_to?(:to_i)
+      end
+
+      def self.wrap(thing)
+        if thing.is_a?(Flipper::Types::Actor)
+          thing
+        else
+          new(thing)
+        end
+      end
+
       attr_reader :identifier
 
-      def initialize(identifier)
-        @identifier = identifier.to_i
+      def initialize(thing)
+        raise ArgumentError, "thing cannot be nil" if thing.nil?
+
+        @thing = thing
+        @identifier = if thing.respond_to?(:identifier)
+          thing.identifier
+        elsif thing.respond_to?(:id)
+          thing.id
+        else
+          thing
+        end.to_i
       end
 
       def enabled_value
@@ -12,6 +36,14 @@ module Flipper
       end
 
       alias_method :disabled_value, :enabled_value
+
+      def respond_to?(*args)
+        super || @thing.respond_to?(*args)
+      end
+
+      def method_missing(name, *args, &block)
+        @thing.send name, *args, &block
+      end
     end
   end
 end
--- spec/flipper/types/actor_spec.rb
+++ spec/flipper/types/actor_spec.rb
@@ -3,21 +3,119 @@ require 'flipper/types/actor'
 
 describe Flipper::Types::Actor do
   subject {
-    Flipper::Types::Actor.new(2)
+    described_class.new(2)
   }
 
+  let(:thing_class) {
+    Class.new {
+      attr_reader :identifier
+
+      def initialize(identifier)
+        @identifier = identifier
+      end
+
+      def admin?
+        true
+      end
+    }
+  }
+
+  describe ".wrappable?" do
+    it "returns true if actor" do
+      thing = described_class.new(1)
+      described_class.wrappable?(thing).should be_true
+    end
+
+    it "returns true if responds to identifier" do
+      thing = Struct.new(:identifier).new(10)
+      described_class.wrappable?(thing).should be_true
+    end
+
+    it "returns true if responds to id" do
+      thing = Struct.new(:id).new(10)
+      described_class.wrappable?(thing).should be_true
+    end
+
+    it "returns true if responds to to_i" do
+      described_class.wrappable?(1).should be_true
+    end
+
+    it "returns false if not actor and does not respond to identifier, id, nor to_i" do
+      described_class.wrappable?(:nope).should be_false
+    end
+  end
+
+  describe ".wrap" do
+    context "for actor" do
+      it "returns actor" do
+        actor = described_class.wrap(subject)
+        actor.should be_instance_of(described_class)
+        actor.should be(subject)
+      end
+    end
+
+    context "for other thing" do
+      it "returns actor" do
+        actor = described_class.wrap(1)
+        actor.should be_instance_of(described_class)
+      end
+    end
+  end
+
   it "initializes with identifier" do
-    actor = Flipper::Types::Actor.new(2)
-    actor.should be_instance_of(Flipper::Types::Actor)
+    actor = described_class.new(2)
+    actor.should be_instance_of(described_class)
+  end
+
+  it "initializes with object that responds to identifier" do
+    thing = Struct.new(:identifier).new(1)
+    actor = described_class.new(thing)
+    actor.identifier.should be(1)
+  end
+
+  it "initializes with object that responds to id" do
+    thing = Struct.new(:id).new(13)
+    actor = described_class.new(thing)
+    actor.identifier.should be(13)
+  end
+
+  it "raises error when initialized with nil" do
+    expect {
+      described_class.new(nil)
+    }.to raise_error(ArgumentError)
   end
 
   it "converts identifier to integer" do
-    actor = Flipper::Types::Actor.new('2')
+    actor = described_class.new('2')
     actor.identifier.should eq(2)
   end
 
   it "has identifier" do
-    actor = Flipper::Types::Actor.new(2)
+    actor = described_class.new(2)
     actor.identifier.should eq(2)
+  end
+
+  it "proxies everything to thing" do
+    thing = thing_class.new(10)
+    actor = described_class.new(thing)
+    actor.admin?.should be_true
+  end
+
+  describe "#respond_to?" do
+    it "returns true if responds to method" do
+      actor = described_class.new(10)
+      actor.respond_to?(:enabled_value).should be_true
+    end
+
+    it "returns true if thing responds to method" do
+      thing = thing_class.new(10)
+      actor = described_class.new(thing)
+      actor.respond_to?(:admin?).should be_true
+    end
+
+    it "returns false if does not respond to method and thing does not respond to method" do
+      actor = described_class.new(10)
+      actor.respond_to?(:frankenstein).should be_false
+    end
   end
 end
