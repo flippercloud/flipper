19d596c258dda313cded53c3cdc7a6bd17549971
John Nunemaker <nunemaker@gmail.com>
2012-09-16 14:49:46 -0400

    Add feature specs that were in integration back to feature

---
+++ spec/flipper/feature_spec.rb
@@ -0,0 +1,36 @@
+require 'helper'
+require 'flipper/feature'
+require 'flipper/adapters/memory'
+
+describe Flipper::Feature do
+  subject           { described_class.new(:search, adapter) }
+
+  let(:source)      { {} }
+  let(:adapter)     { Flipper::Adapters::Memory.new(source) }
+
+  it "initializes with name and adapter" do
+    feature = described_class.new(:search, adapter)
+    feature.name.should eq(:search)
+    feature.adapter.should eq(Flipper::Adapter.wrap(adapter))
+  end
+
+  describe "#gates" do
+    it "returns array of gates" do
+      subject.gates.should be_instance_of(Array)
+      subject.gates.each do |gate|
+        gate.should be_a(Flipper::Gate)
+      end
+      subject.gates.size.should be(5)
+    end
+  end
+
+  context "#disabled?" do
+    it "returns the opposite of enabled" do
+      subject.stub(:enabled? => true)
+      subject.disabled?.should be_false
+
+      subject.stub(:enabled? => false)
+      subject.disabled?.should be_true
+    end
+  end
+end
