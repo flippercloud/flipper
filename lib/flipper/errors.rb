c3b47c60da143a4118b0e07711e35091010205e0
John Nunemaker <nunemaker@gmail.com>
2013-01-14 14:13:49 -0500

    Document errors.

--- lib/flipper/errors.rb
+++ lib/flipper/errors.rb
@@ -1,11 +1,14 @@
 module Flipper
+  # Top level error that all other errors inherit from.
   class Error < StandardError; end
 
+  # Raised when gate can not be found for a thing.
   class GateNotFound < Error
     def initialize(thing)
       super "Could not find gate for #{thing.inspect}"
     end
   end
 
+  # Raised when attempting to declare a group name that has already been used.
   class DuplicateGroup < Error; end
 end
