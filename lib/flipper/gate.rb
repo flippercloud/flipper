2d9b28f92de257f77f52a18f7ff22f29707d6159
John Nunemaker <nunemaker@gmail.com>
2013-01-14 14:16:52 -0500

    Document gate.

--- lib/flipper/gate.rb
+++ lib/flipper/gate.rb
@@ -19,42 +19,61 @@ module Flipper
       @instrumentor = options.fetch(:instrumentor, Flipper::Instrumentors::Noop)
     end
 
+    # Public: The name of the gate. Implemented in subclass.
     def name
       raise 'Not implemented'
     end
 
+    # Private: The key used in the adapter for the gate. Implemented in subclass.
     def type_key
       raise 'Not implemented'
     end
 
+    # Internal: The key where details about this gate can be retrieved from the
+    # adapter.
     def key
       @key ||= Key.new(@feature.name, type_key)
     end
 
+    # Internal: The toggle class to use for this gate.
     def toggle_class
       Toggles::Value
     end
 
+    # Internal: The toggle to use to enable/disable this gate.
     def toggle
       @toggle ||= toggle_class.new(self)
     end
 
+    # Internal: Check if a gate is open for a thing. Implemented in subclass.
+    #
+    # Returns true if gate open for thing, false if not.
     def open?(thing)
       false
     end
 
+    # Internal: Check if a gate is protects a thing. Implemented in subclass.
+    #
+    # Returns true if gate protects thing, false if not.
     def protects?(thing)
       false
     end
 
+    # Internal: Enable this gate for a thing.
+    #
+    # Returns the result of Flipper::Toggle#enable.
     def enable(thing)
       toggle.enable(thing)
     end
 
+    # Internal: Disable this gate for a thing.
+    #
+    # Returns the result of Flipper::Toggle#disable.
     def disable(thing)
       toggle.disable(thing)
     end
 
+    # Public: Pretty string version for debugging.
     def inspect
       attributes = [
         "feature=#{feature.name.inspect}",
