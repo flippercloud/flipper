7e4a3731253fd152f847fa172119769e6812ff2e
John Nunemaker <nunemaker@gmail.com>
2013-02-13 11:36:31 -0500

    Remove toggle from boolean gate.

--- lib/flipper/gates/boolean.rb
+++ lib/flipper/gates/boolean.rb
@@ -1,6 +1,32 @@
 module Flipper
   module Gates
     class Boolean < Gate
+      TruthMap = {
+        true    => true,
+        'true'  => true,
+        'TRUE'  => true,
+        'True'  => true,
+        't'     => true,
+        'T'     => true,
+        '1'     => true,
+        'on'    => true,
+        'ON'    => true,
+        1       => true,
+        1.0     => true,
+        false   => false,
+        'false' => false,
+        'FALSE' => false,
+        'False' => false,
+        'f'     => false,
+        'F'     => false,
+        '0'     => false,
+        'off'   => false,
+        'OFF'   => false,
+        0       => false,
+        0.0     => false,
+        nil     => false,
+      }
+
       # Internal: The name of the gate. Used for instrumentation, etc.
       def name
         :boolean
@@ -11,9 +37,25 @@ module Flipper
         :boolean
       end
 
-      # Internal: The toggle class used to enable/disable the gate for a thing.
-      def toggle_class
-        Toggles::Boolean
+      def enable(thing)
+        adapter.write adapter_key, thing.value
+        true
+      end
+
+      def disable(thing)
+        feature.gates.each do |gate|
+          gate.adapter.delete gate.adapter_key
+        end
+        true
+      end
+
+      def enabled?
+        value
+      end
+
+      def value
+        value = adapter.read(adapter_key)
+        !!TruthMap[value]
       end
 
       # Internal: Checks if the gate is open for a thing.
