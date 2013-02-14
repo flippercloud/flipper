2caa89b09b6026c2f315d312cdf81bfe34d09a71
John Nunemaker <nunemaker@gmail.com>
2013-02-14 16:01:26 -0500

    Added value to boolean type.

--- spec/flipper/types/boolean_spec.rb
+++ spec/flipper/types/boolean_spec.rb
@@ -2,8 +2,18 @@ require 'helper'
 require 'flipper/types/boolean'
 
 describe Flipper::Types::Boolean do
-  it "initializes with nothing" do
-    switch = Flipper::Types::Boolean.new
-    switch.should be_instance_of(Flipper::Types::Boolean)
+  it "defaults value to true" do
+    boolean = Flipper::Types::Boolean.new
+    boolean.value.should be(true)
+  end
+
+  it "allows overriding default value" do
+    boolean = Flipper::Types::Boolean.new(false)
+    boolean.value.should be(false)
+  end
+
+  it "returns true for nil value" do
+    boolean = Flipper::Types::Boolean.new(nil)
+    boolean.value.should be(true)
   end
 end
