8d8c763473bc0cbe1aa646bc8f79b6b7fbccee11
John Nunemaker <nunemaker@gmail.com>
2013-02-19 14:53:39 -0500

    Document using memoizer middleware.

--- lib/flipper/middleware/memoizer.rb
+++ lib/flipper/middleware/memoizer.rb
@@ -3,6 +3,22 @@ require 'rack/body_proxy'
 module Flipper
   module Middleware
     class Memoizer
+      # Public: Initializes an instance of the UI middleware.
+      #
+      # app - The app this middleware is included in.
+      # flipper_or_block - The Flipper::DSL instance or a block that yields a
+      #                    Flipper::DSL instance to use for all operations.
+      #
+      # Examples
+      #
+      #   flipper = Flipper.new(...)
+      #
+      #   # using with a normal flipper instance
+      #   use Flipper::Middleware::Memoizer, flipper
+      #
+      #   # using with a block that yields a flipper instance
+      #   use Flipper::Middleware::Memoizer, lambda { Flipper.new(...) }
+      #
       def initialize(app, flipper_or_block)
         @app = app
 
