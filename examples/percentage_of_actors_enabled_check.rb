825221a2f5af4e16ec46be2660aa7e3c5124c8e3
John Nunemaker <nunemaker@gmail.com>
2015-05-29 10:37:10 -0400

    Add percentage of actors enabled check example

---
+++ examples/percentage_of_actors_enabled_check.rb
@@ -0,0 +1,39 @@
+require File.expand_path('../example_setup', __FILE__)
+
+require 'flipper'
+require 'flipper/adapters/memory'
+
+adapter = Flipper::Adapters::Memory.new
+flipper = Flipper.new(adapter)
+
+# Some class that represents what will be trying to do something
+class User
+  attr_reader :id
+
+  def initialize(id)
+    @id = id
+  end
+
+  # Must respond to flipper_id
+  alias_method :flipper_id, :id
+end
+
+# checking a bunch
+gate = Flipper::Gates::PercentageOfActors.new
+feature_name = "data_migration"
+percentage_enabled = 10
+total = 20_000
+enabled = []
+
+(1..total).each do |id|
+  user = User.new(id)
+  if gate.open?(user, percentage_enabled, feature_name: feature_name)
+    enabled << user
+  end
+end
+
+p actual: enabled.size, expected: total * (percentage_enabled * 0.01)
+
+# checking one
+user = User.new(1)
+p user_1_enabled: Flipper::Gates::PercentageOfActors.new.open?(user, percentage_enabled, feature_name: feature_name)
