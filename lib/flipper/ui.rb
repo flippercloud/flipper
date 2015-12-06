717085dae96d9d2be2c411e09e344af76fde7884
John Nunemaker <nunemaker@gmail.com>
2015-12-05 20:46:24 -0500

    Add docs for app crumb href option

--- lib/flipper/ui.rb
+++ lib/flipper/ui.rb
@@ -12,6 +12,9 @@ require 'flipper/ui/middleware'
 module Flipper
   module UI
     class << self
+      # Public: If you set this, the UI will always have a first breadcrumb that
+      # says "App" which points to this href. The href can be a path (ie: "/")
+      # or full url ("https://app.example.com/").
       attr_accessor :application_breadcrumb_href
     end
 
