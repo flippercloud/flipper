fbf99a9a7a25a92765afbf0b3631e67956ec2412
John Nunemaker <nunemaker@gmail.com>
2016-02-26 09:16:35 -0500

    Get rid of magic in operation logger

--- lib/flipper/adapters/operation_logger.rb
+++ lib/flipper/adapters/operation_logger.rb
@@ -33,14 +33,47 @@ module Flipper
         @operations = operations || []
       end
 
-      # Wraps original method with in memory log of operations performed.
-      OperationTypes.each do |type|
-        class_eval <<-EOE
-          def #{type}(*args)
-            @operations << Operation.new(:#{type}, args)
-            @adapter.send(:#{type}, *args)
-          end
-        EOE
+      # Public: The set of known features.
+      def features
+        @operations << Operation.new(:features, [])
+        @adapter.features
+      end
+
+      # Public: Adds a feature to the set of known features.
+      def add(feature)
+        @operations << Operation.new(:add, [feature])
+        @adapter.add(feature)
+      end
+
+      # Public: Removes a feature from the set of known features and clears
+      # all the values for the feature.
+      def remove(feature)
+        @operations << Operation.new(:remove, [feature])
+        @adapter.remove(feature)
+      end
+
+      # Public: Clears all the gate values for a feature.
+      def clear(feature)
+        @operations << Operation.new(:clear, [feature])
+        @adapter.clear(feature)
+      end
+
+      # Public
+      def get(feature)
+        @operations << Operation.new(:get, [feature])
+        @adapter.get(feature)
+      end
+
+      # Public
+      def enable(feature, gate, thing)
+        @operations << Operation.new(:enable, [feature, gate, thing])
+        @adapter.enable(feature, gate, thing)
+      end
+
+      # Public
+      def disable(feature, gate, thing)
+        @operations << Operation.new(:disable, [feature, gate, thing])
+        @adapter.disable(feature, gate, thing)
       end
 
       # Public: Count the number of times a certain operation happened.
