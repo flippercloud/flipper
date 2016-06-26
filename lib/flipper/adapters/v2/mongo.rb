4f77288ab8f7cb0b9d7b5bbd0571bacb27750c9a
John Nunemaker <nunemaker@gmail.com>
2016-06-26 09:50:52 -0400

    Add mongo v2 adapter

---
+++ lib/flipper/adapters/v2/mongo.rb
@@ -0,0 +1,39 @@
+module Flipper
+  module Adapters
+    module V2
+      class Mongo
+        include ::Flipper::Adapter
+
+        attr_reader :name
+
+        def initialize(collection)
+          @collection = collection
+          @name = :mongo
+        end
+
+        def version
+          Adapter::V2
+        end
+
+        def get(key)
+          criteria = {:_id => key.to_s}
+          if doc = @collection.find(criteria).limit(1).first
+            doc["value"]
+          end
+        end
+
+        def set(key, value)
+          criteria = {:_id => key.to_s}
+          options = {:upsert => true}
+          updates = {'$set' => {"value" => value.to_s}}
+          @collection.find(criteria).update_one(updates, options)
+        end
+
+        def del(key)
+          criteria = {:_id => key.to_s}
+          @collection.find(criteria).delete_one
+        end
+      end
+    end
+  end
+end
