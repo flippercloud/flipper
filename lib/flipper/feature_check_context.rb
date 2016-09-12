81d59e992c971d5918cf59a4a24513fbfda42f6c
John Nunemaker <nunemaker@gmail.com>
2016-09-12 09:05:24 -0400

    Add docs for feature check context methods

--- lib/flipper/feature_check_context.rb
+++ lib/flipper/feature_check_context.rb
@@ -1,6 +1,10 @@
 module Flipper
   class FeatureCheckContext
+    # Public: The name of the feature.
     attr_reader :feature_name
+
+    # Public: The GateValues instance that keeps track of the values for the
+    # gates for the feature.
     attr_reader :values
 
     def initialize(feature_name:, values:)
