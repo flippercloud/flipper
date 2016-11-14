5c250f415c202fed4c886fb9f157ff4b51d78bb2
John Nunemaker <nunemaker@gmail.com>
2016-11-14 11:34:56 -0500

    More flexible value column data type

--- test/generators/flipper/active_record_v2_generator_test.rb
+++ test/generators/flipper/active_record_v2_generator_test.rb
@@ -15,7 +15,7 @@ class CreateFlipperV2Tables < ActiveRecord::Migration
   def self.up
     create_table :flipper_keys do |t|
       t.string :key, null: false
-      t.string :value, null: false
+      t.binary :value, null: false
       t.timestamps null: false
     end
     add_index :flipper_keys, :key, unique: true
