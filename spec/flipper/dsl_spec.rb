98b586b5847779ffc4ef3c501a3838961e137ce0
Garry Shutler <garry@robustsoftware.co.uk>
2016-11-19 21:13:39 +0000

    Adds specs for DSL#preload

--- spec/flipper/dsl_spec.rb
+++ spec/flipper/dsl_spec.rb
@@ -37,6 +37,39 @@ RSpec.describe Flipper::DSL do
     end
   end
 
+  describe "#preload" do
+    let(:instrumenter) { double('Instrumentor', :instrument => nil) }
+    let(:dsl) { Flipper::DSL.new(adapter, :instrumenter => instrumenter) }
+    let(:names) { %i{stats shiny} }
+    let(:features) { dsl.preload(names) }
+
+    it "returns array of features" do
+      expect(features).to all be_instance_of(Flipper::Feature)
+    end
+
+    it "sets names" do
+      expect(features.map(&:name)).to eq(names)
+    end
+
+    it "sets adapter" do
+      features.each do |feature|
+        expect(feature.adapter.name).to eq(dsl.adapter.name)
+      end
+    end
+
+    it "sets instrumenter" do
+      features.each do |feature|
+        expect(feature.instrumenter).to eq(dsl.instrumenter)
+      end
+    end
+
+    it "memoizes the feature" do
+      features.each do |feature|
+        expect(dsl.feature(feature.name)).to equal(feature)
+      end
+    end
+  end
+
   describe "#[]" do
     it_should_behave_like "a DSL feature" do
       let(:method_name) { :[] }
