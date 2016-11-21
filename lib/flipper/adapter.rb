b408fa04922783f1aed7488b1da83347e02d84ba
John Nunemaker <nunemaker@gmail.com>
2016-11-21 12:13:38 -0500

    self is implicit so removing it

--- lib/flipper/adapter.rb
+++ lib/flipper/adapter.rb
@@ -2,7 +2,7 @@ module Flipper
   module Adapter
     # adding a module include so we have some hooks for stuff down the road
     def get_multi(features)
-      features.map { |feature| self.get(feature) }
+      features.map { |feature| get(feature) }
     end
   end
 end
