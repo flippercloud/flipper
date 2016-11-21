f4f8d5511d106e9c42644bad3d13cb21a092f17d
John Nunemaker <nunemaker@gmail.com>
2016-11-21 12:19:24 -0500

    Add doc notes about get multi method in adapter

--- lib/flipper/adapter.rb
+++ lib/flipper/adapter.rb
@@ -1,6 +1,9 @@
 module Flipper
+  # Adding a module include so we have some hooks for stuff down the road
   module Adapter
-    # adding a module include so we have some hooks for stuff down the road
+    # Public: Get multiple features in one call. Defaults to one get per
+    # feature. Feel free to override per adapter to make this more efficient and
+    # reduce network calls.
     def get_multi(features)
       features.map { |feature| get(feature) }
     end
