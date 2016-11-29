818aa1b8918fb394a1ed001945764b72fb8c333a
John Nunemaker <nunemaker@gmail.com>
2016-11-28 22:03:16 -0500

    Wrap up key generation for dalli in one place

--- lib/flipper/adapters/dalli.rb
+++ lib/flipper/adapters/dalli.rb
@@ -41,27 +41,28 @@ module Flipper
       def remove(feature)
         result = @adapter.remove(feature)
         @cache.delete(FeaturesKey)
-        @cache.delete(feature)
+        @cache.delete(key_for(feature))
         result
       end
 
       # Public
       def clear(feature)
         result = @adapter.clear(feature)
-        @cache.delete(feature)
+        @cache.delete(key_for(feature))
         result
       end
 
       # Public
       def get(feature)
-        @cache.fetch(feature, @ttl) do
+        @cache.fetch(key_for(feature), @ttl) do
           @adapter.get(feature)
         end
       end
 
       def get_multi(features)
-        result = @cache.get_multi(features)
-        uncached_features = features.reject { |feature| result[feature] }
+        keys = features.map { |feature| key_for(feature) }
+        result = @cache.get_multi(keys)
+        uncached_features = features.reject { |feature| result[key_for(feature)] }
 
         if uncached_features.any?
           response = @adapter.get_multi(uncached_features)
@@ -77,15 +78,21 @@ module Flipper
       # Public
       def enable(feature, gate, thing)
         result = @adapter.enable(feature, gate, thing)
-        @cache.delete(feature)
+        @cache.delete(key_for(feature))
         result
       end
 
       # Public
       def disable(feature, gate, thing)
         result = @adapter.disable(feature, gate, thing)
-        @cache.delete(feature)
+        @cache.delete(key_for(feature))
         result
+      end
+
+      private
+
+      def key_for(feature)
+        feature
       end
     end
   end
