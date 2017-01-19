7fb1a8d458d75cd0394b32661179dbdc21d1cfd5
Alex Wheeler <afwheeler92@gmail.com>
2017-01-19 07:34:25 -0500

    Add request module
    * this module can handle all http request logic
    * any classes that want to make requests can mix this in
    * with this addition move request logic out of Http and into Request

--- lib/flipper/adapters/http.rb
+++ lib/flipper/adapters/http.rb
@@ -3,13 +3,37 @@ require 'json'
 
 module Flipper
   module Adapters
+    # Module for handling http requests.
+    # Any class that needs to make an http request can include/use this
+    module Request
+      HEADERS = { 'Content-Type' => 'application/json' }.freeze
+
+      def get_request(path)
+        uri = URI.parse(path)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Get.new(uri.request_uri, HEADERS)
+        response = http.request(request)
+      end
+
+      def post_request(path, data)
+        uri = URI.parse(path)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri, HEADERS)
+        request.body = data.to_json
+        response = http.request(request)
+      end
+
+      def delete_request(path)
+        uri = URI.parse(path)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Delete.new(uri.request_uri, HEADERS)
+        response = http.request(request)
+      end
+    end
+
     class Http
-      include  Flipper::Adapter
-
-      HEADERS = {
-        'Content-Type' =>  'application/json'
-      }.freeze
-
+      include Flipper::Adapter
+      include Request
       attr_reader :name
 
       def initialize(path_to_mount)
@@ -17,63 +41,56 @@ module Flipper
         @name = :http
       end
 
+      # Get one feature
       def get(feature)
-        url = endpoint("/api/v1/features/#{feature}")
-        res = http.get(url)
-        JSON.parse(res.body)
+        response = get_request(@path + "/api/v1/features/#{feature}")
+        # JSON.parse(response.body)
       end
 
+      # Add a feature
       def add(feature)
-        uri = URI.parse(@path + '/api/v1/features')
-        http = Net::HTTP.new(uri.host, uri.port)
-        response = http.post(uri.path, {name: feature}.to_json, HEADERS)
-        puts JSON.parse(response.body)
+        response = post_request(@path + '/api/v1/features', name: feature)
+        # JSON.parse(response.body)
       end
 
       def get_multi(features)
         # could be cool to add this feature as an api endpoint requesting multiple features
-        []
+        # or alternatively use a persistent connection and request multiple endpoints
       end
 
+      # Get all features
       def features
-        url = endpoint("/api/v1/features")
-        res = http.get(url)
+        response = get_request(@path + '/api/v1/features')
+        # JSON.parse(response.body)
       end
 
+      # Remove a feature
       def remove(feature)
-        url = endpoint("/api/v1/features/#{feature}")
-        res = http.delete(url)
+        response = delete_request(@path + "/api/v1/features/#{feature}")
+        # JSON.parse(response.body)
       end
 
+      # Enable gate thing for feature
       def enable(feature, gate, thing)
-        url = endpoint("/api/v1/features/#{feature.key}/#{gate.key}")
-        body = request_body(gate.key, thing.value.to_s)
-        res = http.post(url, body)
+        body = gate_request_body(gate.key, thing.value.to_s)
+        response = post_request(@path + "/api/v1/features/#{feature.key}/#{gate.key}", body)
+        # JSON.parse(response)
       end
 
-      def disable(feature, gate, thing)
-        url = endpoint("/api/v1/features/#{feature.key}/#{gate.key}")
-        res = http.delete(url)
+      # Disable gate thing for feature
+      def disable(feature, gate, _thing)
+        response = delete_request(@path + "/api/v1/features/#{feature.key}/#{gate.key}")
+        # JSON.parse(response)
       end
 
       private
 
-      def http
-       Net::HTTP.new(@path)
-      end
-
-      def request(url)
-        uri = URI.parse(@path + url)
-        Net::HTTP.new(uri)
-      end
-
-      def endpoint(url)
-        "#{@path}#{url}"
-      end
-
-      def request_body(gate_key, value)
+      # Returns request body for enabling/disabling a gate
+      # i.e gate_request_body(:percentage_of_actors, 10)
+      # returns { 'percentage' => 10 }
+      def gate_request_body(gate_key, value)
         parameter = gate_parameter(gate_key)
-        {"#{parameter}" => value}.to_json
+        { parameter.to_s => value }
       end
 
       def gate_parameter(gate_name)
@@ -84,10 +101,10 @@ module Flipper
           :flipper_id
         when :percentage_of_actors
           :percentage
-        when :percentage_of_timec
+        when :percentage_of_time
           :percentage
         else
-          raise "#{gate_name} is not a valid gate name"}
+          raise "#{gate_name} is not a valid flipper gate name"
         end
       end
     end
