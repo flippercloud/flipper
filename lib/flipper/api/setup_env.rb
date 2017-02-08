054e83c391b7eeea18e0b3c428db46777bb11f10
John Nunemaker <nunemaker@gmail.com>
2017-02-07 20:46:51 -0500

    Add setup env middleware

---
+++ lib/flipper/api/setup_env.rb
@@ -0,0 +1,44 @@
+module Flipper
+  module Api
+    class SetupEnv
+      # Public: Initializes an instance of the SetEnv middleware. Allows for
+      # lazy initialization of the flipper instance being set in the env by
+      # providing a block.
+      #
+      # app - The app this middleware is included in.
+      # flipper_or_block - The Flipper::DSL instance or a block that yields a
+      #                    Flipper::DSL instance to use for all operations.
+      #
+      # Examples
+      #
+      #   flipper = Flipper.new(...)
+      #
+      #   # using with a normal flipper instance
+      #   use Flipper::Api::SetEnv, flipper
+      #
+      #   # using with a block that yields a flipper instance
+      #   use Flipper::Api::SetEnv, lambda { Flipper.new(...) }
+      #
+      def initialize(app, flipper_or_block)
+        @app = app
+
+        if flipper_or_block.respond_to?(:call)
+          @flipper_block = flipper_or_block
+        else
+          @flipper = flipper_or_block
+        end
+      end
+
+      def call(env)
+        env["flipper".freeze] ||= flipper
+        @app.call(env)
+      end
+
+      private
+
+      def flipper
+        @flipper ||= @flipper_block.call
+      end
+    end
+  end
+end
---
+++ spec/flipper/api/setup_env_spec.rb
@@ -0,0 +1,58 @@
+require 'helper'
+
+RSpec.describe Flipper::Api::SetupEnv do
+  context "with flipper instance" do
+    let(:app) do
+      app = lambda do |env|
+        [200, { 'Content-Type' => 'text/html' }, [env["flipper"].object_id.to_s]]
+      end
+      builder = Rack::Builder.new
+      builder.use described_class, flipper
+      builder.run app
+      builder
+    end
+
+    it "sets flipper in env" do
+      get "/"
+      expect(last_response.body).to eq(flipper.object_id.to_s)
+    end
+  end
+
+  context "with block that returns flipper instance" do
+    let(:flipper_block) {
+      ->{ flipper }
+    }
+    let(:app) do
+      app = lambda do |env|
+        [200, { 'Content-Type' => 'text/html' }, [env["flipper"].object_id.to_s]]
+      end
+      builder = Rack::Builder.new
+      builder.use described_class, flipper_block
+      builder.run app
+      builder
+    end
+
+    it "sets flipper in env" do
+      get "/"
+      expect(last_response.body).to eq(flipper.object_id.to_s)
+    end
+  end
+
+  context "when env already has flipper setup" do
+    let(:app) do
+      app = lambda do |env|
+        [200, { 'Content-Type' => 'text/html' }, [env["flipper"].object_id.to_s]]
+      end
+      builder = Rack::Builder.new
+      builder.use described_class, flipper
+      builder.run app
+      builder
+    end
+
+    it "leaves env flipper alone" do
+      env_flipper = build_flipper
+      get "/", {}, {"flipper" => env_flipper}
+      expect(last_response.body).to eq(env_flipper.object_id.to_s)
+    end
+  end
+end
