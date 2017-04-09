2ebd802d13efcd258567d04f827f4911698f683d
John Nunemaker <nunemaker@gmail.com>
2017-04-09 11:10:09 -0400

    Add spec for migration multiple features

--- spec/flipper/adapter_spec.rb
+++ spec/flipper/adapter_spec.rb
@@ -2,38 +2,59 @@ require 'helper'
 require 'flipper/adapters/memory'
 
 RSpec.describe Flipper::Adapter do
-  let(:actor) { Struct.new(:flipper_id).new('22') }
+  let(:actor_class) { Struct.new(:flipper_id) }
 
   let(:source_flipper) { build_flipper }
   let(:destination_flipper) { build_flipper }
 
   it 'can migrate from one adapter to another' do
     source_flipper.enable(:search)
-    source_flipper.enable_group(:admins, :admins)
-    source_flipper.enable_actor(:debug, actor)
-    source_flipper.enable_percentage_of_actors(:issues, 25)
-    source_flipper.enable_percentage_of_time(:logging, 50)
-    source_flipper.enable(:nope)
-    source_flipper.disable(:nope)
-
     destination_flipper.migrate(source_flipper)
-
-    expect(destination_flipper[:search].gate_values).to eq(source_flipper[:search].gate_values)
-    expect(destination_flipper[:admin].gate_values).to eq(source_flipper[:admin].gate_values)
-    expect(destination_flipper[:debug].gate_values).to eq(source_flipper[:debug].gate_values)
-    expect(destination_flipper[:issues].gate_values).to eq(source_flipper[:issues].gate_values)
-    expect(destination_flipper[:logging].gate_values).to eq(source_flipper[:logging].gate_values)
-    expect(destination_flipper[:nope].gate_values).to eq(source_flipper[:nope].gate_values)
-    expected_feature_keys = %w[search admins debug issues logging nope].sort
-    expect(destination_flipper.features.map(&:key).sort).to eq(expected_feature_keys)
+    expect(destination_flipper[:search].boolean_value).to eq(true)
+    expect(destination_flipper.features.map(&:key).sort).to eq(%w[search])
   end
 
   it 'can migrate features that exist but are off' do
     feature = source_flipper[:search]
     source_flipper.add(:search)
+    destination_flipper.migrate(source_flipper)
+    expect(destination_flipper.features.map(&:key)).to eq(["search"])
+  end
+
+  it 'can migrate multiple features' do
+    source_flipper.enable(:yep)
+    source_flipper.enable_group(:preview_features, :developers)
+    source_flipper.enable_group(:preview_features, :marketers)
+    source_flipper.enable_group(:preview_features, :company)
+    source_flipper.enable_group(:preview_features, :early_access)
+    source_flipper.enable_actor(:preview_features, actor_class.new('1'))
+    source_flipper.enable_actor(:preview_features, actor_class.new('2'))
+    source_flipper.enable_actor(:preview_features, actor_class.new('3'))
+    source_flipper.enable_percentage_of_actors(:issues_next, 25)
+    source_flipper.enable_percentage_of_time(:verbose_logging, 5)
 
     destination_flipper.migrate(source_flipper)
 
-    expect(destination_flipper.features.map(&:key)).to eq(["search"])
+    feature = destination_flipper[:preview_features]
+    expect(feature.boolean_value).to eq(false)
+    expect(feature.actors_value).to eq(Set['1', '2', '3'])
+    expected_groups = Set['developers', 'marketers', 'company', 'early_access']
+    expect(feature.groups_value).to eq(expected_groups)
+    expect(feature.percentage_of_actors_value).to eq(0)
+    expect(feature.percentage_of_time_value).to eq(0)
+
+    feature = destination_flipper[:issues_next]
+    expect(feature.boolean_value).to eq(false)
+    expect(feature.actors_value).to eq(Set.new)
+    expect(feature.groups_value).to eq(Set.new)
+    expect(feature.percentage_of_actors_value).to eq(25)
+    expect(feature.percentage_of_time_value).to eq(0)
+
+    feature = destination_flipper[:verbose_logging]
+    expect(feature.boolean_value).to eq(false)
+    expect(feature.actors_value).to eq(Set.new)
+    expect(feature.groups_value).to eq(Set.new)
+    expect(feature.percentage_of_actors_value).to eq(0)
+    expect(feature.percentage_of_time_value).to eq(5)
   end
 end
