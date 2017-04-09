9f31dfcfc2290d7da7aa3b4a73a8101efb97a531
John Nunemaker <nunemaker@gmail.com>
2017-04-09 11:33:49 -0400

    Check == for actor as well

--- spec/flipper/actor_spec.rb
+++ spec/flipper/actor_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe Flipper::Actor do
       expect(actor1.eql?(actor2)).to be(false)
     end
   end
+
+  describe '#==' do
+    it 'returns true if same class and flipper_id' do
+      actor1 = described_class.new("User;235")
+      actor2 = described_class.new("User;235")
+      expect(actor1.==(actor2)).to be(true)
+    end
+
+    it 'returns false if same class but different flipper_id' do
+      actor1 = described_class.new("User;235")
+      actor2 = described_class.new("User;1")
+      expect(actor1.==(actor2)).to be(false)
+    end
+
+    it 'returns false for different class' do
+      actor1 = described_class.new("User;235")
+      actor2 = Struct.new(:flipper_id).new("User;235")
+      expect(actor1.==(actor2)).to be(false)
+    end
+  end
 end
