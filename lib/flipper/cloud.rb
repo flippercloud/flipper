e5d4f96f67378696977946497299e21098cb16b1
John Nunemaker <nunemaker@gmail.com>
2017-05-06 14:50:39 -0400

    No need for fetch block
    It is just a string.

--- lib/flipper/cloud.rb
+++ lib/flipper/cloud.rb
@@ -4,7 +4,7 @@ require "flipper/instrumenters/noop"
 module Flipper
   module Cloud
     def self.new(token, options = {})
-      url = options.fetch(:url) { "https://www.featureflipper.com/adapter" }
+      url = options.fetch(:url, "https://www.featureflipper.com/adapter")
       instrumenter = options.fetch(:instrumenter, Flipper::Instrumenters::Noop)
       adapter = Flipper::Adapters::Http.new(uri: URI(url),
                                             headers: {
