17ffbec943bdf223220075e9f1c9f45d4ffaca28
Alex Wheeler <afwheeler92@gmail.com>
2017-06-09 08:34:40 -0400

    Accept expires_in CacheStore contructor over options hash
    * Before we accepted all options passed them onto the cache methods, but
    this allows us to be more specific
    
    * use a write_options hash since we don't want to override the default
    expires_in if its already been set on the cache instance

--- lib/flipper/adapters/cache_store.rb
+++ lib/flipper/adapters/cache_store.rb
@@ -22,16 +22,17 @@ module Flipper
       attr_reader :name
 
       # Public
-      def initialize(adapter, cache, options = nil)
+      def initialize(adapter, cache, expires_in: nil)
         @adapter = adapter
         @name = :cache_store
         @cache = cache
-        @options = options
+        @write_options = {}
+        @write_options.merge!(expires_in: expires_in) if expires_in
       end
 
       # Public
       def features
-        @cache.fetch(FeaturesKey, @options) do
+        @cache.fetch(FeaturesKey, @write_options) do
           @adapter.features
         end
       end
@@ -39,28 +40,28 @@ module Flipper
       # Public
       def add(feature)
         result = @adapter.add(feature)
-        @cache.delete(FeaturesKey, @options)
+        @cache.delete(FeaturesKey)
         result
       end
 
       ## Public
       def remove(feature)
         result = @adapter.remove(feature)
-        @cache.delete(FeaturesKey, @options)
-        @cache.delete(key_for(feature.key), @options)
+        @cache.delete(FeaturesKey)
+        @cache.delete(key_for(feature.key))
         result
       end
 
       ## Public
       def clear(feature)
         result = @adapter.clear(feature)
-        @cache.delete(key_for(feature.key), @options)
+        @cache.delete(key_for(feature.key))
         result
       end
 
       ## Public
       def get(feature)
-        @cache.fetch(key_for(feature.key), @options) do
+        @cache.fetch(key_for(feature.key), @write_options) do
           @adapter.get(feature)
         end
       end
@@ -72,7 +73,7 @@ module Flipper
         if uncached_features.any?
           response = @adapter.get_multi(uncached_features)
           response.each do |key, value|
-            @cache.write(key_for(key), value, @options)
+            @cache.write(key_for(key), value, @write_options)
             result[key] = value
           end
         end
@@ -82,14 +83,14 @@ module Flipper
       ## Public
       def enable(feature, gate, thing)
         result = @adapter.enable(feature, gate, thing)
-        @cache.delete(key_for(feature.key), @options)
+        @cache.delete(key_for(feature.key))
         result
       end
 
       ## Public
       def disable(feature, gate, thing)
         result = @adapter.disable(feature, gate, thing)
-        @cache.delete(key_for(feature.key), @options)
+        @cache.delete(key_for(feature.key))
         result
       end
 
