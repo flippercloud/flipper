b393f4466cc833d7aa6d02154922d90a610aaba8
John Nunemaker <nunemaker@gmail.com>
2017-07-24 09:47:29 -0400

    Docs for config

--- lib/flipper/configuration.rb
+++ lib/flipper/configuration.rb
@@ -4,10 +4,18 @@ module Flipper
       @default = lambda { raise DefaultNotSet }
     end
 
+    # Controls the default instance for flipper. Defaults to raising an error.
+    #
+    # Flipper::Configuration.new.default do
+    #   require "flipper/adapters/memory"
+    #   Flipper.new(Flipper::Adapters::Memory.new)
+    # end
     def default(&block)
       @default = block
     end
 
+    # Public: Returns the result of default block invocation, which should
+    # always be a Flipper::DSL instance. This can be set with default method.
     def default_instance
       @default.call
     end
