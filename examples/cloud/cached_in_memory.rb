cfa51af9c180ba65e86c869d55f6e912c3336e46
John Nunemaker <nunemaker@gmail.com>
2017-10-21 14:29:09 -0400

    Added in memory cache example for flipper cloud

---
+++ examples/cloud/cached_in_memory.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../../example_setup', __FILE__)
+
+require 'flipper/cloud'
+require 'flipper/adapters/cache_store'
+require 'active_support/cache'
+require 'active_support/cache/memory_store'
+
+token = ENV.fetch("TOKEN") { abort "TOKEN environment variable not set." }
+feature_name = ENV.fetch("FEATURE") { "testing" }.to_sym
+
+Flipper.configure do |config|
+  config.default do
+    Flipper::Cloud.new(token, {
+      debug_output: STDOUT,
+      adapter_wrapper: ->(adapter) {
+        cache = ActiveSupport::Cache::MemoryStore.new
+        Flipper::Adapters::CacheStore.new(adapter, cache, {expires_in: 5.seconds})
+      },
+    })
+  end
+end
+
+loop do
+  # Should only print out http call every 10 seconds
+  p Flipper.enabled?(feature_name)
+  puts "\n\n"
+
+  sleep 1
+end
