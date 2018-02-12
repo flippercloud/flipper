e3890ef96d316e89f6690faec6b0c5fe0057af1d
John Nunemaker <nunemaker@gmail.com>
2018-02-11 22:33:02 -0500

    Move sync adapter helper classes to their own files so things stay tidy
    Need to get some units for each of them as well.

---
+++ lib/flipper/adapters/sync/interval_synchronizer.rb
@@ -0,0 +1,36 @@
+module Flipper
+  module Adapters
+    class Sync
+      # TODO: Make this instrument when it actually calls the wrapped synchronizer.
+      class IntervalSynchronizer
+        # Private: Default to syncing every 10 seconds.
+        DEFAULT_INTERVAL_MS = 10_000
+
+        def initialize(synchronizer, interval: nil)
+          @synchronizer = synchronizer
+          @interval = interval || DEFAULT_INTERVAL_MS
+          @last_sync_at = 0
+        end
+
+        def call
+          return unless time_to_sync?
+
+          @last_sync_at = now_ms
+          @synchronizer.call
+
+          nil
+        end
+
+        private
+
+        def time_to_sync?
+          (now_ms - @last_sync_at) >= @interval
+        end
+
+        def now_ms
+          Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
+        end
+      end
+    end
+  end
+end
---
+++ lib/flipper/adapters/sync/synchronizer.rb
@@ -0,0 +1,38 @@
+require "flipper/adapters/sync/feature_synchronizer"
+
+module Flipper
+  module Adapters
+    class Sync
+      class Synchronizer
+        def initialize(local, remote, options = {})
+          @local = local
+          @remote = remote
+          @instrumenter = options.fetch(:instrumenter, Instrumenters::Noop)
+        end
+
+        def call
+          local_get_all = @local.get_all
+          remote_get_all = @remote.get_all
+
+          # Sync all the gate values.
+          remote_get_all.each do |feature_key, remote_gates_hash|
+            feature = Feature.new(feature_key, @local)
+            local_gates_hash = local_get_all[feature_key] || @local.default_config
+            local_gate_values = GateValues.new(local_gates_hash)
+            remote_gate_values = GateValues.new(remote_gates_hash)
+            FeatureSynchronizer.new(feature, local_gate_values, remote_gate_values).call
+          end
+
+          # Add features that are missing
+          features_to_add = remote_get_all.keys - local_get_all.keys
+          features_to_add.each { |key| Feature.new(key, @local).add }
+        rescue => exception
+          payload = {
+            exception: exception,
+          }
+          @instrumenter.instrument("synchronizer_exception.flipper", payload)
+        end
+      end
+    end
+  end
+end
