27d70a33d659b01df22c388a184342ac187d35d3
John Nunemaker <nunemaker@gmail.com>
2018-02-22 10:05:20 -0500

    Make the specs for feature sync a bit more readable

--- spec/flipper/adapters/sync/feature_synchronizer_spec.rb
+++ spec/flipper/adapters/sync/feature_synchronizer_spec.rb
@@ -15,14 +15,22 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
 
     it "does nothing if local is disabled" do
       feature.disable
+      adapter.reset
+
       described_class.new(feature, feature.gate_values, remote).call
+
       expect(adapter.get(feature).fetch(:boolean)).to be(nil)
+      expect_no_enable_or_disable
     end
 
     it "disables if local is enabled" do
       feature.enable
+      adapter.reset
+
       described_class.new(feature, feature.gate_values, remote).call
+
       expect(adapter.get(feature).fetch(:boolean)).to be(nil)
+      expect_only_disable
     end
   end
 
@@ -31,14 +39,20 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
 
     it "does nothing if local boolean enabled" do
       feature.enable
+      adapter.reset
+
       described_class.new(feature, feature.gate_values, remote).call
       expect(feature.boolean_value).to be(true)
+      expect_no_enable_or_disable
     end
 
     it "enables if local is disabled" do
       feature.disable
+      adapter.reset
+
       described_class.new(feature, feature.gate_values, remote).call
       expect(feature.boolean_value).to be(true)
+      expect_only_enable
     end
   end
 
@@ -46,20 +60,24 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
     it "adds remotely added actors" do
       remote = Flipper::GateValues.new(actors: Set["1", "2"])
       feature.enable_actor(Flipper::Actor.new("1"))
+      adapter.reset
 
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.actors_value).to eq(Set["1", "2"])
+      expect_only_enable
     end
 
     it "removes remotely removed actors" do
       remote = Flipper::GateValues.new(actors: Set["1"])
       feature.enable_actor(Flipper::Actor.new("1"))
       feature.enable_actor(Flipper::Actor.new("2"))
+      adapter.reset
 
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.actors_value).to eq(Set["1"])
+      expect_only_disable
     end
 
     it "does nothing to actors if in sync" do
@@ -70,26 +88,30 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.actors_value).to eq(Set["1"])
-      expect(adapter.count(:enable)).to be(0)
+      expect_no_enable_or_disable
     end
 
     it "adds remotely added groups" do
       remote = Flipper::GateValues.new(groups: Set["staff", "early_access"])
       feature.enable_group(:staff)
+      adapter.reset
 
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.groups_value).to eq(Set["staff", "early_access"])
+      expect_only_enable
     end
 
     it "removes remotely removed groups" do
       remote = Flipper::GateValues.new(groups: Set["staff"])
       feature.enable_group(:staff)
       feature.enable_group(:early_access)
+      adapter.reset
 
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.groups_value).to eq(Set["staff"])
+      expect_only_disable
     end
 
     it "does nothing if groups in sync" do
@@ -100,7 +122,7 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.groups_value).to eq(Set["staff"])
-      expect(adapter.count(:enable)).to be(0)
+      expect_no_enable_or_disable
     end
 
     it "updates percentage of actors when remote is updated" do
@@ -111,7 +133,7 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.percentage_of_actors_value).to be(25)
-      expect(adapter.count(:enable)).to be(1)
+      expect_only_enable
     end
 
     it "does nothing if local percentage of actors matches remote" do
@@ -122,7 +144,7 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.percentage_of_actors_value).to be(25)
-      expect(adapter.count(:enable)).to be(0)
+      expect_no_enable_or_disable
     end
 
     it "updates percentage of time when remote is updated" do
@@ -133,7 +155,7 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.percentage_of_time_value).to be(25)
-      expect(adapter.count(:enable)).to be(1)
+      expect_only_enable
     end
 
     it "does nothing if local percentage of time matches remote" do
@@ -144,7 +166,22 @@ RSpec.describe Flipper::Adapters::Sync::FeatureSynchronizer do
       described_class.new(feature, feature.gate_values, remote).call
 
       expect(feature.percentage_of_time_value).to be(25)
-      expect(adapter.count(:enable)).to be(0)
+      expect_no_enable_or_disable
     end
+  end
+
+  def expect_no_enable_or_disable
+    expect(adapter.count(:enable)).to be(0)
+    expect(adapter.count(:disable)).to be(0)
+  end
+
+  def expect_only_enable
+    expect(adapter.count(:enable)).to be(1)
+    expect(adapter.count(:disable)).to be(0)
+  end
+
+  def expect_only_disable
+    expect(adapter.count(:enable)).to be(0)
+    expect(adapter.count(:disable)).to be(1)
   end
 end
