affa4d1e84f3cfaaa772d95010c474220d0478c5
John Nunemaker <nunemaker@gmail.com>
2018-04-12 10:59:16 -0400

    Docs for synchronizer

--- lib/flipper/adapters/sync/synchronizer.rb
+++ lib/flipper/adapters/sync/synchronizer.rb
@@ -6,15 +6,23 @@ require "flipper/adapters/sync/feature_synchronizer"
 module Flipper
   module Adapters
     class Sync
-      # Internal: Given a local and remote adapter, it can update the local to
+      # Public: Given a local and remote adapter, it can update the local to
       # match the remote doing only the necessary enable/disable operations.
       class Synchronizer
+        # Public: Initializes a new synchronizer.
+        #
+        # local - The Flipper adapter to get in sync with the remote.
+        # remote - The Flipper adapter that is source of truth that the local
+        #          adapter should be brought in line with.
+        # options - The Hash of options.
+        #           :instrumenter - The instrumenter used to instrument.
         def initialize(local, remote, options = {})
           @local = local
           @remote = remote
           @instrumenter = options.fetch(:instrumenter, Instrumenters::Noop)
         end
 
+        # Public: Forces a sync.
         def call
           @instrumenter.instrument("synchronizer_call.flipper") { sync }
         end
@@ -23,8 +31,6 @@ module Flipper
 
         def sync
           local_get_all = @local.get_all
-          # TODO: Move remote get all to background thread to minimize impact to
-          # whatever is happening in main thread.
           remote_get_all = @remote.get_all
 
           # Sync all the gate values.
