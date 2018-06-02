60f420e49fbc8346dad5399376739c2aa1a4737a
Alex Wheeler <afwheeler92@gmail.com>
2018-06-01 18:23:05 -0700

    Uses namespaced key

--- lib/flipper/adapters/moneta.rb
+++ lib/flipper/adapters/moneta.rb
@@ -1,12 +1,11 @@
 require 'moneta'
-require 'pry'
 
 module Flipper
   module Adapters
     class Moneta
       include ::Flipper::Adapter
 
-      FEATURES_KEY = :features
+      FEATURES_KEY = :flipper_features
 
       # Public: The name of the adapter.
       attr_reader :name
@@ -17,97 +16,108 @@ module Flipper
         @name = :moneta
       end
 
+      # Public:  The set of known features
       def features
-        moneta[:features] || Set.new
+        moneta[FEATURES_KEY] || Set.new
       end
 
       # Public: Adds a feature to the set of known features.
       def add(feature)
-        moneta[:features] = moneta[:features] || Set.new
-        moneta[:features] = moneta[:features] << feature.key.to_s
+        moneta[FEATURES_KEY] = (moneta[FEATURES_KEY] || Set.new) << feature.key.to_s
         true
       end
 
       # Public: Removes a feature from the set of known features and clears
       # all the values for the feature.
       def remove(feature)
-        moneta[:features] = moneta[:features] || Set.new
-        features = moneta[:features]
+        features = moneta[FEATURES_KEY] || Set.new
         features.delete(feature.key.to_s)
-        moneta[:features] = features
-        moneta[feature.key] = default_config
+        moneta[FEATURES_KEY] = features
+        moneta[key(feature.key)] = default_config
         true
       end
 
       # Public: Clears all the gate values for a feature.
       def clear(feature)
-        moneta[feature.key] = default_config
+        moneta[key(feature.key)] = default_config
         true
       end
 
-      # Public
+      # Public: Gets the values for all gates for a given feature.
+      #
+      # Returns a Hash of Flipper::Gate#key => value.
       def get(feature)
-        moneta[feature.key] || default_config
-      end
-
-      def get_multi(features)
-        h = {}
-        features.each do |feature|
-          h[feature.key] = default_config.merge(moneta[feature.key].to_h)
-        end
-        h
-      end
-
-      def get_all
-        h = {}
-        moneta[:features].each do |feature_key|
-          h[feature_key] = default_config.merge(moneta[feature_key].to_h)
-        end
-        h
+        moneta[key(feature.key)] || default_config
       end
 
       # Public
+      def get_multi(features)
+        result = {}
+        features.each do |feature|
+          result[feature.key] = default_config.merge(moneta[key(feature.key)].to_h)
+        end
+        result
+      end
+
+      # Public
+      def get_all
+        result = {}
+        moneta[FEATURES_KEY].each do |feature_key|
+          result[feature_key] = default_config.merge(moneta[key(feature_key)].to_h)
+        end
+        result
+      end
+
+      # Public: Enables a gate for a given thing.
+      #
+      # feature - The Flipper::Feature for the gate.
+      # gate - The Flipper::Gate to disable.
+      # thing - The Flipper::Type being enabled for the gate.
+      #
+      # Returns true.
       def enable(feature, gate, thing)
         case gate.data_type
         when :boolean, :integer
-          the_feature = moneta[feature.key] || {}
-          the_feature[gate.key] = thing.value.to_s
-          moneta[feature.key] = the_feature
+          result = moneta[key(feature.key)] || {}
+          result[gate.key] = thing.value.to_s
+          moneta[key(feature.key)] = result
         when :set
-          the_feature = moneta[feature.key] || {}
-          if the_feature[gate.key]
-            the_feature[gate.key] << thing.value.to_s
-            moneta[feature.key] = the_feature
-          else
-            the_feature[gate.key] = Set.new
-            the_feature[gate.key] << thing.value.to_s
-            moneta[feature.key] = the_feature
-          end
+          result = moneta[key(feature.key)] || {}
+          result[gate.key] ||= Set.new
+          result[gate.key] << thing.value.to_s
+          moneta[key(feature.key)] = result
         end
         true
       end
 
-      # Public
+      # Public: Disables a gate for a given thing.
+      #
+      # feature - The Flipper::Feature for the gate.
+      # gate - The Flipper::Gate to disable.
+      # thing - The Flipper::Type being disabled for the gate.
+      #
+      # Returns true.
       def disable(feature, gate, thing)
-        #binding.pry if thing.value.to_s == 0.to_s
         case gate.data_type
         when :boolean
-          moneta[feature.key] = default_config
+          moneta[key(feature.key)] = default_config
         when :integer
-          the_feature = moneta[feature.key] || default_config
-          the_feature[gate.key] = thing.value.to_s
-          moneta[feature.key] = the_feature
+          result = moneta[key(feature.key)] || {}
+          result[gate.key] = thing.value.to_s
+          moneta[key(feature.key)] = result
         when :set
-          the_feature = moneta[feature.key]
-          if the_feature[gate.key]
-            the_feature[gate.key] = the_feature[gate.key].delete(thing.value.to_s)
-          end
-          moneta[feature.key] = the_feature
+          result = moneta[key(feature.key)]
+          result[gate.key] = result[gate.key].delete(thing.value.to_s) if result[gate.key]
+          moneta[key(feature.key)] = result
         end
         true
       end
 
       private
+
+      def key(feature_key)
+        "#{FEATURES_KEY}/#{feature_key}"
+      end
 
       attr_reader :moneta
     end
