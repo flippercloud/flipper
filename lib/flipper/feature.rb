d46d1e571d49d984da55ae3544033c8c0b0db269
mtodzo <milestodzo@gmail.com>
2019-06-13 10:20:46 -0700

    disable rubocop class length metric

--- lib/flipper/feature.rb
+++ lib/flipper/feature.rb
@@ -5,6 +5,7 @@ require 'flipper/feature_check_context'
 require 'flipper/gate_values'
 
 module Flipper
+  # rubocop:disable Metrics/ClassLength
   class Feature
     # Private: The name of feature instrumentation events.
     InstrumentationName = "feature_operation.#{InstrumentationNamespace}".freeze
