0b9e1ca0d524d3dc4f3cabf914ebdc84a5d6485b
John Nunemaker <nunemaker@gmail.com>
2019-09-13 14:13:25 -0400

    Fix flipper ui rackup to take env for default app

--- lib/flipper/ui.rb
+++ lib/flipper/ui.rb
@@ -42,7 +42,7 @@ module Flipper
     def self.app(flipper = nil, options = {})
       env_key = options.fetch(:env_key, 'flipper')
       rack_protection_options = options.fetch(:rack_protection, use: :authenticity_token)
-      app = ->() { [200, { 'Content-Type' => 'text/html' }, ['']] }
+      app = ->(env) { [200, { 'Content-Type' => 'text/html' }, ['']] }
       builder = Rack::Builder.new
       yield builder if block_given?
       builder.use Rack::Protection, rack_protection_options
