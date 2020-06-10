ef819f3f286920479c6304be4328e7f180ef187b
John Nunemaker <nunemaker@gmail.com>
2020-06-10 12:25:16 -0400

    Fix failing spec for removed method

--- spec/flipper/ui/decorators/feature_spec.rb
+++ spec/flipper/ui/decorators/feature_spec.rb
@@ -22,56 +22,6 @@ RSpec.describe Flipper::UI::Decorators::Feature do
     end
   end
 
-  describe '#as_json' do
-    before do
-      @result = subject.as_json
-    end
-
-    it 'returns Hash' do
-      expect(@result).to be_instance_of(Hash)
-    end
-
-    it 'includes id' do
-      expect(@result['id']).to eq('some_awesome_feature')
-    end
-
-    it 'includes pretty name' do
-      expect(@result['name']).to eq('Some Awesome Feature')
-    end
-
-    describe 'description' do
-      context 'when a value is present' do
-        subject do
-          instance = described_class.new(feature)
-          instance.description = 'Awesome feature description'
-          instance
-        end
-
-        it 'includes it' do
-          expect(@result['description']).to eq('Awesome feature description')
-        end
-      end
-
-      context 'when a value is not present' do
-        it 'includes it' do
-          expect(@result['description']).to eq(nil)
-        end
-      end
-    end
-
-    it 'includes state' do
-      expect(@result['state']).to eq('off')
-    end
-
-    it 'includes gates' do
-      gates = subject.gates.map do |gate|
-        value = subject.gate_values[gate.key]
-        Flipper::UI::Decorators::Gate.new(gate, value).as_json
-      end
-      expect(@result['gates']).to eq(gates)
-    end
-  end
-
   describe '#<=>' do
     let(:on) do
       flipper.enable(:on_a)
