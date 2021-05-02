b119cede92fed2e12298d866d8ec54b6040cf905
Brandon Keepers <brandon@opensoul.org>
2021-05-02 15:34:05 -0400

    Tests for mounting of cloud webhook app

--- spec/flipper/cloud/engine_spec.rb
+++ spec/flipper/cloud/engine_spec.rb
@@ -4,10 +4,7 @@ require 'flipper/cloud'
 
 RSpec.describe Flipper::Cloud::Engine do
   let(:env) do
-    {
-      "FLIPPER_CLOUD_TOKEN" => "ASDF",
-      "FLIPPER_CLOUD_SYNC_SECRET" => "abc"
-    }
+    { "FLIPPER_CLOUD_TOKEN" => "ASDF" }
   end
 
   let(:application) do
@@ -32,11 +29,36 @@ RSpec.describe Flipper::Cloud::Engine do
     end
   end
 
-  it "configures webhook app" do
-    with_modified_env env do
-      application.initialize!
-
-      # TOOD: test this…
+  context "with CLOUD_SYNC_SECRET" do
+    before do
+      env.update "FLIPPER_CLOUD_SYNC_SECRET" => "abc"
     end
+
+    it "configures webhook app" do
+      with_modified_env env do
+        application.initialize!
+
+        expect(find_route("/_flipper")).to be_a(ActionDispatch::Journey::Route)
+      end
+    end
+  end
+
+  context "without CLOUD_SYNC_SECRET" do
+    it "does not configure webhook app" do
+      with_modified_env env do
+        application.initialize!
+
+        expect(find_route("/_flipper")).to be(nil)
+      end
+    end
+  end
+
+  def find_route(path)
+    # `routes.recognize_path` doesn't work with rack apps, so find route manually
+    req = ActionDispatch::Request.new(Rack::MockRequest.env_for(path, method: "GET"))
+    matched_route = nil
+    application.routes.router.recognize(req) { |route,_| matched_route ||= route }
+
+    matched_route
   end
 end
