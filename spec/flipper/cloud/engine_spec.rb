33643fafbcc01d505c032839f0c3e8f10e547cac
Brandon Keepers <brandon@opensoul.org>
2021-05-04 15:48:12 -0400

    Move stub that was obscuring boot issue

--- spec/flipper/cloud/engine_spec.rb
+++ spec/flipper/cloud/engine_spec.rb
@@ -17,13 +17,13 @@ RSpec.describe Flipper::Cloud::Engine do
   before do
     Rails.application = nil
 
-    stub_request(:get, /flippercloud\.io/).to_return(status: 200, body: "{}")
-
     # Force loading of flipper to configure itself
     load 'flipper-cloud.rb'
   end
 
   it "initializes cloud configuration" do
+    stub_request(:get, /flippercloud\.io/).to_return(status: 200, body: "{}")
+
     with_modified_env env do
       expect(Flipper.instance).to be_a(Flipper::Cloud::DSL)
     end
