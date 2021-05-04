0da06bf4dff1efa5ed0705f145eb1d6189509550
Brandon Keepers <brandon@opensoul.org>
2021-05-04 10:53:42 -0400

    Configure cloud routes in initializers

--- lib/flipper/cloud/engine.rb
+++ lib/flipper/cloud/engine.rb
@@ -3,19 +3,17 @@ require "flipper/railtie"
 module Flipper
   module Cloud
     class Engine < Rails::Engine
-      initializer "flipper.cloud" do
-        config.after_initialize do |app|
-          cloud_config = Flipper.configuration.default.cloud_configuration
+      initializer "flipper.cloud", after: :load_config_initializers do |app|
+        cloud_config = Flipper.configuration.default.cloud_configuration
 
-          if cloud_config.sync_method == :webhook
-            cloud_app = Flipper::Cloud.app(
-              env_key: Flipper.configuration.env_key,
-              memoizer_options: { preload: Flipper.configuration.preload }
-            )
+        if cloud_config.sync_method == :webhook
+          cloud_app = Flipper::Cloud.app(
+            env_key: Flipper.configuration.env_key,
+            memoizer_options: { preload: Flipper.configuration.preload }
+          )
 
-            app.routes.draw do
-              mount cloud_app, at: cloud_config.app_path
-            end
+          app.routes.draw do
+            mount cloud_app, at: cloud_config.app_path
           end
         end
       end
