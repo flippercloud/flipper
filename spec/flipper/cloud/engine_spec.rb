65b9fc9934dfe0e2ed186cc21e0023617aedf4b7
John Nunemaker <nunemaker@gmail.com>
2021-05-06 19:22:46 -0400

    Fix flipper cloud load specs

--- spec/flipper/cloud/engine_spec.rb
+++ spec/flipper/cloud/engine_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Flipper::Cloud::Engine do
     Rails.application = nil
 
     # Force loading of flipper to configure itself
-    load 'flipper/adapters/cloud.rb'
+    load 'flipper/cloud.rb'
   end
 
   it "initializes cloud configuration" do
