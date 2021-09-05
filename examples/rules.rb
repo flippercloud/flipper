d711954bddb0ac30cf9286a71ac74c0e0c5e96ed
John Nunemaker <nunemaker@gmail.com>
2021-09-05 13:39:20 -0400

    Add more complex examples of rules

--- examples/rules.rb
+++ examples/rules.rb
@@ -8,24 +8,85 @@ end
 user = User.new(1, {
   "plan" => "basic",
   "age" => 39,
+  "roles" => ["team_user"]
 })
 
-p Flipper.enabled?(:something, user)
-any_rule = Flipper::Rules::Any.new(
-  Flipper::Rules::Condition.new(
-    {"type" => "property", "value" => "plan"},
-    {"type" => "operator", "value" => "eq"},
-    {"type" => "string", "value" => "basic"}
-  ),
-  Flipper::Rules::Condition.new(
-    {"type" => "property", "value" => "age"},
-    {"type" => "operator", "value" => "gte"},
-    {"type" => "integer", "value" => 21}
-  )
+admin_user = User.new(2, {
+  "roles" => ["admin", "team_user"],
+})
+
+other_user = User.new(3, {
+  "plan" => "plus",
+  "age" => 18,
+  "roles" => ["org_admin"]
+})
+
+age_rule = Flipper::Rules::Condition.new(
+  {"type" => "property", "value" => "age"},
+  {"type" => "operator", "value" => "gte"},
+  {"type" => "integer", "value" => 21}
+)
+plan_rule = Flipper::Rules::Condition.new(
+  {"type" => "property", "value" => "plan"},
+  {"type" => "operator", "value" => "eq"},
+  {"type" => "string", "value" => "basic"}
+)
+admin_rule = Flipper::Rules::Condition.new(
+  {"type" => "string", "value" => "admin"},
+  {"type" => "operator", "value" => "in"},
+  {"type" => "property", "value" => "roles"}
 )
 
-Flipper.enable_rule :something, any_rule
-p Flipper.enabled?(:something, user)
+puts "Single Rule"
+p should_be_false: Flipper.enabled?(:something, user)
+puts "Enabling single rule"
+Flipper.enable_rule :something, plan_rule
+p should_be_true: Flipper.enabled?(:something, user)
+p should_be_false: Flipper.enabled?(:something, admin_user)
+p should_be_false: Flipper.enabled?(:something, other_user)
+puts "Disabling single rule"
+Flipper.disable_rule :something, plan_rule
+p should_be_false: Flipper.enabled?(:something, user)
 
+
+puts "\n\nAny Rule"
+p should_be_false: Flipper.enabled?(:something, user)
+any_rule = Flipper::Rules::Any.new(plan_rule, age_rule)
+puts "Enabling any rule"
+Flipper.enable_rule :something, any_rule
+p should_be_true: Flipper.enabled?(:something, user)
+p should_be_false: Flipper.enabled?(:something, admin_user)
+p should_be_false: Flipper.enabled?(:something, other_user)
+puts "Disabling any rule"
 Flipper.disable_rule :something, any_rule
-p Flipper.enabled?(:something, user)
+p should_be_false: Flipper.enabled?(:something, user)
+
+
+puts "\n\nAll Rule"
+p should_be_false: Flipper.enabled?(:something, user)
+all_rule = Flipper::Rules::All.new(plan_rule, age_rule)
+puts "Enabling all rule"
+Flipper.enable_rule :something, all_rule
+p should_be_true: Flipper.enabled?(:something, user)
+p should_be_false: Flipper.enabled?(:something, admin_user)
+p should_be_false: Flipper.enabled?(:something, other_user)
+puts "Disabling all rule"
+Flipper.disable_rule :something, all_rule
+p should_be_false: Flipper.enabled?(:something, user)
+
+
+puts "\n\nNested Rule"
+nested_rule = Flipper::Rules::Any.new(admin_rule, all_rule)
+p should_be_false: Flipper.enabled?(:something, user)
+p should_be_false: Flipper.enabled?(:something, admin_user)
+p should_be_false: Flipper.enabled?(:something, other_user)
+puts "Enabling nested rule"
+Flipper.enable_rule :something, nested_rule
+p should_be_true: Flipper.enabled?(:something, user)
+p should_be_true: Flipper.enabled?(:something, admin_user)
+p should_be_false: Flipper.enabled?(:something, other_user)
+puts "Disabling nested rule"
+Flipper.disable_rule :something, nested_rule
+p should_be_false: Flipper.enabled?(:something, user)
+p should_be_false: Flipper.enabled?(:something, admin_user)
+p should_be_false: Flipper.enabled?(:something, other_user)
