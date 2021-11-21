ea499ee4e8fc41bea80d00d410e5a5f00c709dfe
John Nunemaker <nunemaker@gmail.com>
2021-11-20 20:44:35 -0500

    Add property evaluation specs to equal

--- spec/flipper/expressions/equal_spec.rb
+++ spec/flipper/expressions/equal_spec.rb
@@ -11,28 +11,54 @@ RSpec.describe Flipper::Expressions::Equal do
 
     expect(expression).to be_instance_of(Flipper::Expressions::Equal)
     expect(expression.args).to eq([
-      Flipper::Expressions::Value.new(["basic"]),
-      Flipper::Expressions::Value.new(["basic"]),
+      Flipper.value("basic"),
+      Flipper.value("basic"),
     ])
   end
 
   describe "#evaluate" do
     it "returns true when equal" do
       expression = described_class.new([
-        Flipper::Expressions::Value.new(["basic"]),
-        Flipper::Expressions::Value.new(["basic"]),
+        Flipper.value("basic"),
+        Flipper.value("basic"),
       ])
 
       expect(expression.evaluate).to be(true)
     end
 
+    it "returns true when properties equal" do
+      expression = described_class.new([
+        Flipper.property(:first),
+        Flipper.property(:second),
+      ])
+
+      properties = {
+        "first" => "foo",
+        "second" => "foo",
+      }
+      expect(expression.evaluate(properties: properties)).to be(true)
+    end
+
     it "returns false when not equal" do
       expression = described_class.new([
-        Flipper::Expressions::Value.new(["basic"]),
-        Flipper::Expressions::Value.new(["plus"]),
+        Flipper.value("basic"),
+        Flipper.value("plus"),
       ])
 
       expect(expression.evaluate).to be(false)
+    end
+
+    it "returns false when properties not equal" do
+      expression = described_class.new([
+        Flipper.property(:first),
+        Flipper.property(:second),
+      ])
+
+      properties = {
+        "first" => "foo",
+        "second" => "bar",
+      }
+      expect(expression.evaluate(properties: properties)).to be(false)
     end
   end
 
@@ -40,7 +66,7 @@ RSpec.describe Flipper::Expressions::Equal do
     it "returns Hash" do
       expression = described_class.new([
         Flipper::Expressions::Property.new(["plan"]),
-        Flipper::Expressions::Value.new(["basic"]),
+        Flipper.value("basic"),
       ])
 
       expect(expression.value).to eq({
