fe25249064f20012b74f197bce5cf888caf85a1c
John Nunemaker <nunemaker@hey.com>
2021-12-21 11:33:36 -0500

    Update lib/flipper/ui/action.rb
    Add trailing comma back.

--- lib/flipper/ui/action.rb
+++ lib/flipper/ui/action.rb
@@ -24,7 +24,7 @@ module Flipper
                                              'get'.freeze,
                                              'post'.freeze,
                                              'put'.freeze,
-                                             'delete'.freeze
+                                             'delete'.freeze,
                                            ]).freeze
 
       SOURCES = {
