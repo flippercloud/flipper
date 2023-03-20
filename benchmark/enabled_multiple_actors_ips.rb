49212320912e82b64c77c8696f8f0349a08004b9
John Nunemaker <nunemaker@gmail.com>
2023-03-20 15:20:52 -0400

    Fix benchmark language

--- benchmark/enabled_multiple_actors_ips.rb
+++ benchmark/enabled_multiple_actors_ips.rb
@@ -14,7 +14,7 @@ actor8 = Flipper::Actor.new("User;8")
 actors = [actor1, actor2, actor3, actor4, actor5, actor6, actor7, actor8]
 
 Benchmark.ips do |x|
-  x.report("with 5 actors") { Flipper.enabled?(:foo, actors) }
-  x.report("with 5 enabled? checks") { actors.each { |actor| Flipper.enabled?(:foo, actor) } }
+  x.report("with array of actors") { Flipper.enabled?(:foo, actors) }
+  x.report("with multiple enabled? checks") { actors.each { |actor| Flipper.enabled?(:foo, actor) } }
   x.compare!
 end
