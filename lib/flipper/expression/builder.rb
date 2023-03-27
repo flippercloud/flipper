4d9a5c9a72ba70c74623744e8f50e432085abb00
Brandon Keepers <brandon@opensoul.org>
2023-03-27 11:29:26 -0400

    Remove unnecessary class reference

--- lib/flipper/expression/builder.rb
+++ lib/flipper/expression/builder.rb
@@ -14,47 +14,47 @@ module Flipper
       end
 
       def any
-        any? ? self : Expression.build({ Any: [self] })
+        any? ? self : build({ Any: [self] })
       end
 
       def all
-        all? ? self : Expression.build({ All: [self] })
+        all? ? self : build({ All: [self] })
       end
 
       def equal(object)
-        Expression.build({ Equal: [self, object] })
+        build({ Equal: [self, object] })
       end
       alias eq equal
 
       def not_equal(object)
-        Expression.build({ NotEqual: [self, object] })
+        build({ NotEqual: [self, object] })
       end
       alias neq not_equal
 
       def greater_than(object)
-        Expression.build({ GreaterThan: [self, object] })
+        build({ GreaterThan: [self, object] })
       end
       alias gt greater_than
 
       def greater_than_or_equal_to(object)
-        Expression.build({ GreaterThanOrEqualTo: [self, object] })
+        build({ GreaterThanOrEqualTo: [self, object] })
       end
       alias gte greater_than_or_equal_to
       alias greater_than_or_equal greater_than_or_equal_to
 
       def less_than(object)
-        Expression.build({ LessThan: [self, object] })
+        build({ LessThan: [self, object] })
       end
       alias lt less_than
 
       def less_than_or_equal_to(object)
-        Expression.build({ LessThanOrEqualTo: [self, object] })
+        build({ LessThanOrEqualTo: [self, object] })
       end
       alias lte less_than_or_equal_to
       alias less_than_or_equal less_than_or_equal_to
 
       def percentage_of_actors(object)
-        Expression.build({ PercentageOfActors: [self, Expression.build(object)] })
+        build({ PercentageOfActors: [self, build(object)] })
       end
 
       def any?
