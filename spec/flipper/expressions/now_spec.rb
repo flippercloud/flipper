56cbdaa35a4f4dee21faccf1d18648afbcd64de3
Brandon Keepers <brandon@opensoul.org>
2023-03-29 11:51:19 -0400

    Allow time to be flexible
    Co-authored-by: John Nunemaker <nunemaker@gmail.com>

--- spec/flipper/expressions/now_spec.rb
+++ spec/flipper/expressions/now_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Flipper::Expressions::Now do
   describe "#call" do
     it "returns current time" do
-      expect(described_class.call.round).to eq(Time.now.round)
+      expect(described_class.call).to be_within(2).of(Time.now.utc)
     end
 
     it "defaults to UTC" do
