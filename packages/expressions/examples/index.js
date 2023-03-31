4c35f3e33d3f3e5ca5a4a88ebd721c5f3fe68f6b
Brandon Keepers <brandon@opensoul.org>
2023-03-31 13:12:16 -0400

    Run JS lint/tests

--- packages/expressions/examples/index.js
+++ packages/expressions/examples/index.js
@@ -1,6 +1,6 @@
 import { basename } from 'path'
 
-const modules = import.meta.glob("./*.json", { eager: true, import: 'default' })
+const modules = import.meta.glob('./*.json', { eager: true, import: 'default' })
 
 export default Object.fromEntries(Object.entries(modules).map(([path, module]) => {
   return [basename(path, '.json'), module]
--- packages/expressions/schemas/index.js
+++ packages/expressions/schemas/index.js
@@ -1,7 +1,7 @@
 const modules = import.meta.glob('./*.json', { eager: true, import: 'default' })
 const schemas = Object.fromEntries(Object.entries(modules).map(([path, module]) => {
-  const name = path.split('/').pop().split('.').shift();
-  return [name == 'schema' ? 'default' : name, module];
-}));
+  const name = path.split('/').pop().split('.').shift()
+  return [name === 'schema' ? 'default' : name, module]
+}))
 
-export default schemas;
+export default schemas
