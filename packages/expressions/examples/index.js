6262795f41d2f4010c408d76ed4367c5a3ba2217
Brandon Keepers <brandon@opensoul.org>
2023-04-01 07:25:02 -0400

    Remove use of path

--- packages/expressions/examples/index.js
+++ packages/expressions/examples/index.js
@@ -1,7 +1,6 @@
-import { basename } from 'path'
-
 const modules = import.meta.glob('./*.json', { eager: true, import: 'default' })
 
 export default Object.fromEntries(Object.entries(modules).map(([path, module]) => {
-  return [basename(path, '.json'), module]
+  const name = path.split('/').pop().split('.').shift()
+  return [name, module]
 }))
