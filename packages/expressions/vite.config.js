8f69da49f72d42495283aa2dfd4781dee9ecaef8
Brandon Keepers <brandon@opensoul.org>
2023-04-10 08:50:49 -0400

    Build sourcemaps

--- packages/expressions/vite.config.js
+++ packages/expressions/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'lib/index.js'),
       name: '@flippercloud.io/expressions'
