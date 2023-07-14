80e70f11ebb8baafbf7197a1002abf2580fd6316
Brandon Keepers <brandon@opensoul.org>
2023-07-14 12:46:36 -0400

    Only configure flipper-cloud if token is present

--- lib/flipper/cloud/engine.rb
+++ lib/flipper/cloud/engine.rb
@@ -10,16 +10,13 @@ module Flipper
       end
 
       initializer "flipper.cloud.default", before: :load_config_initializers do |app|
-        Flipper.configure do |config|
-          config.default do
-            if ENV["FLIPPER_CLOUD_TOKEN"]
+        if ENV["FLIPPER_CLOUD_TOKEN"]
+          Flipper.configure do |config|
+            config.default do
               Flipper::Cloud.new(
                 local_adapter: config.adapter,
                 instrumenter: app.config.flipper.instrumenter
               )
-            else
-              warn "Missing FLIPPER_CLOUD_TOKEN environment variable. Disabling Flipper::Cloud."
-              Flipper.new(config.adapter)
             end
           end
         end
