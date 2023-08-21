d3aedafdee7b17dbcf105a73c0e52a62de270aae
Brandon Keepers <brandon@opensoul.org>
2023-08-21 09:03:38 -0400

    Use yardoc tags for documentation

--- lib/flipper/dsl.rb
+++ lib/flipper/dsl.rb
@@ -4,20 +4,20 @@ module Flipper
   class DSL
     extend Forwardable
 
-    # Private
+    # @internal
     attr_reader :adapter
 
-    # Private: What is being used to instrument all the things.
+    # @internal What is being used to instrument all the things.
     attr_reader :instrumenter
 
+    # @internal
     def_delegators :@adapter, :memoize=, :memoizing?, :import, :export
 
-    # Public: Returns a new instance of the DSL.
+    # Creates a new instance of the DSL.
     #
-    # adapter - The adapter that this DSL instance should use.
-    # options - The Hash of options.
-    #           :instrumenter - What should be used to instrument all the things.
-    #           :memoize - Should adapter be wrapped by memoize adapter or not.
+    # @param adapter [Adapter] The adapter that this DSL instance should use.
+    # @option options [#instrument] :instrumenter What should be used to instrument all the things.
+    # @option options [Boolean] :memoize Should adapter be wrapped by memoize adapter or not.
     def initialize(adapter, options = {})
       @instrumenter = options.fetch(:instrumenter, Instrumenters::Noop)
       memoize = options.fetch(:memoize, true)
@@ -26,156 +26,133 @@ module Flipper
       @memoized_features = {}
     end
 
-    # Public: Check if a feature is enabled.
+    # Check if a feature is enabled.
     #
-    # name - The String or Symbol name of the feature.
-    # args - The args passed through to the enabled check.
-    #
-    # Returns true if feature is enabled, false if not.
+    # @param name [String, Symbol] The name of the feature.
+    # @param args The args passed through to the enabled check.
+    # @return [Boolean] true if feature is enabled, false if not.
     def enabled?(name, *args)
       feature(name).enabled?(*args)
     end
 
-    # Public: Enable a feature.
+    # Enable a feature.
     #
-    # name - The String or Symbol name of the feature.
-    # args - The args passed through to the feature instance enable call.
-    #
-    # Returns the result of the feature instance enable call.
+    # @param name [String, Symbol] The name of the feature.
+    # @param args The args passed through to the feature instance enable call.
+    # @return the result of {Feature#enable}
     def enable(name, *args)
       feature(name).enable(*args)
     end
 
-    # Public: Enable a feature for an actor.
+    # Enable a feature for an actor.
     #
-    # name - The String or Symbol name of the feature.
-    # actor - a Flipper::Types::Actor instance or an object that responds
-    #         to flipper_id.
-    #
-    # Returns result of Feature#enable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param actor [Types::Actor, #flipper_id]
+    # @return [Boolean] the result of {#enable}
     def enable_actor(name, actor)
       feature(name).enable_actor(actor)
     end
 
-    # Public: Enable a feature for a group.
+    # Enable a feature for a group.
     #
-    # name - The String or Symbol name of the feature.
-    # group - a Flipper::Types::Group instance or a String or Symbol name of a
-    #         registered group.
-    #
-    # Returns result of Feature#enable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param group [Types::Group, String, Symbol] The name of a registered group.
+    # @return the result of {Feature#enable}.
     def enable_group(name, group)
       feature(name).enable_group(group)
     end
 
-    # Public: Enable a feature a percentage of time.
+    # Enable a feature a percentage of time.
     #
-    # name - The String or Symbol name of the feature.
-    # percentage - a Flipper::Types::PercentageOfTime instance or an object
-    #              that responds to to_i.
-    #
-    # Returns result of Feature#enable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param percentage [Types::PercentageOfTime, #to_i] The percentage of time to enable the feature.
+    # @return the result of {Feature#enable}.
     def enable_percentage_of_time(name, percentage)
       feature(name).enable_percentage_of_time(percentage)
     end
 
-    # Public: Enable a feature for a percentage of actors.
+    # Enable a feature for a percentage of actors.
     #
-    # name - The String or Symbol name of the feature.
-    # percentage - a Flipper::Types::PercentageOfActors instance or an object
-    #              that responds to to_i.
-    #
-    # Returns result of Feature#enable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param percentage [Types::PercentageOfActors, #to_i] The percentage of actors to enable the feature.
+    # @return the result of {Feature#enable}.
     def enable_percentage_of_actors(name, percentage)
       feature(name).enable_percentage_of_actors(percentage)
     end
 
-    # Public: Disable a feature.
+    # Disable a feature.
     #
-    # name - The String or Symbol name of the feature.
-    # args - The args passed through to the feature instance enable call.
-    #
-    # Returns the result of the feature instance disable call.
+    # @param name [String, Symbol] The name of the feature.
+    # @param args The args passed through to {Feature#disable}
+    # @return the result of {Feature#disable}
     def disable(name, *args)
       feature(name).disable(*args)
     end
 
-    # Public: Disable a feature for an actor.
+    # Disable a feature for an actor.
     #
-    # name - The String or Symbol name of the feature.
-    # actor - a Flipper::Types::Actor instance or an object that responds
-    #         to flipper_id.
-    #
-    # Returns result of disable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param actor [Types::Actor, #flipper_id]
+    # @return the result of {Feature#disable}
     def disable_actor(name, actor)
       feature(name).disable_actor(actor)
     end
 
-    # Public: Disable a feature for a group.
+    # Disable a feature for a group.
     #
-    # name - The String or Symbol name of the feature.
-    # group - a Flipper::Types::Group instance or a String or Symbol name of a
-    #         registered group.
-    #
-    # Returns result of disable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param group [Types::Group, String, Symbol] The name of a registered group.
+    # @return the result of {Feature#disable}
     def disable_group(name, group)
       feature(name).disable_group(group)
     end
 
-    # Public: Disable a feature a percentage of time.
+    # Disable a feature a percentage of time.
     #
-    # name - The String or Symbol name of the feature.
-    # percentage - a Flipper::Types::PercentageOfTime instance or an object
-    #              that responds to to_i.
-    #
-    # Returns result of disable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param percentage [Types::PercentageOfTime, #to_i] The percentage of time to enable the feature.
+    # @return the result of {Feature#disable}
     def disable_percentage_of_time(name)
       feature(name).disable_percentage_of_time
     end
 
-    # Public: Disable a feature for a percentage of actors.
+    # Disable a feature for a percentage of actors.
     #
-    # name - The String or Symbol name of the feature.
-    # percentage - a Flipper::Types::PercentageOfActors instance or an object
-    #              that responds to to_i.
-    #
-    # Returns result of disable.
+    # @param name [String, Symbol] The name of the feature.
+    # @param percentage [Types::PercentageOfActors, #to_i] The percentage of actors to enable the feature.
+    # @return the result of {Feature#disable}
     def disable_percentage_of_actors(name)
       feature(name).disable_percentage_of_actors
     end
 
-    # Public: Add a feature.
+    # Add a feature.
     #
-    # name - The String or Symbol name of the feature.
-    #
-    # Returns result of add.
+    # @param name [String, Symbol] The name of the feature.
+    # @return the result of {Feature#add}
     def add(name)
       feature(name).add
     end
 
-    # Public: Has a feature been added in the adapter.
+    # Has a feature been added in the adapter.
     #
-    # name - The String or Symbol name of the feature.
-    #
-    # Returns true if added else false.
+    # @param name [String, Symbol] The name of the feature.
+    # @return [Boolean] true if added else false.
     def exist?(name)
       feature(name).exist?
     end
 
-    # Public: Remove a feature.
+    # Remove a feature.
     #
-    # name - The String or Symbol name of the feature.
-    #
-    # Returns result of remove.
+    # @param name [String, Symbol] The name of the feature.
+    # @return the result of {Feature#remove}.
     def remove(name)
       feature(name).remove
     end
 
-    # Public: Access a feature instance by name.
+    # Access a feature instance by name.
     #
-    # name - The String or Symbol name of the feature.
-    #
-    # Returns an instance of Flipper::Feature.
+    # @param name [String, Symbol] The name of the feature.
+    # @return [Feature]
     def feature(name)
       if !name.is_a?(String) && !name.is_a?(Symbol)
         raise ArgumentError, "#{name} must be a String or Symbol"
@@ -184,53 +161,52 @@ module Flipper
       @memoized_features[name.to_sym] ||= Feature.new(name, @adapter, instrumenter: instrumenter)
     end
 
-    # Public: Preload the features with the given names.
+    # Preload the features with the given names.
     #
-    # names - An Array of String or Symbol names of the features.
-    #
-    # Returns an Array of Flipper::Feature.
+    # @param names [Array<String,Symbol>] the names of the features
+    # @return [Array<Feature>]
     def preload(names)
       features = names.map { |name| feature(name) }
       @adapter.get_multi(features)
       features
     end
 
-    # Public: Preload all the adapters features.
+    # Preload all the adapters features.
     #
-    # Returns an Array of Flipper::Feature.
+    # @return [Array<Feature>]
     def preload_all
       keys = @adapter.get_all.keys
       keys.map { |key| feature(key) }
     end
 
-    # Public: Shortcut access to a feature instance by name.
+    # Shortcut access to a feature instance by name.
     #
-    # name - The String or Symbol name of the feature.
-    #
-    # Returns an instance of Flipper::Feature.
+    # @param name [String, Symbol] The name of the feature.
+    # @eturn [Feature].
     alias_method :[], :feature
 
-    # Public: Access a flipper group by name.
+    # Access a flipper group by name.
     #
-    # name - The String or Symbol name of the feature.
-    #
-    # Returns an instance of Flipper::Group.
+    # @param name [String, Symbol] The name of the feature.
+    # @return [Group]
     def group(name)
       Flipper.group(name)
     end
 
-    # Public: Returns a Set of the known features for this adapter.
+    # Returns a Set of the known features for this adapter.
     #
-    # Returns Set of Flipper::Feature instances.
+    # @return [Set<Feature>]
     def features
       adapter.features.map { |name| feature(name) }.to_set
     end
 
     # Cloud DSL method that does nothing for open source version.
+    # @internal
     def sync
     end
 
     # Cloud DSL method that does nothing for open source version.
+    # @internal
     def sync_secret
     end
   end
--- lib/flipper/feature.rb
+++ lib/flipper/feature.rb
@@ -6,29 +6,27 @@ require 'flipper/gate_values'
 
 module Flipper
   class Feature
-    # Private: The name of feature instrumentation events.
+    # @internal The name of feature instrumentation events.
     InstrumentationName = "feature_operation.#{InstrumentationNamespace}".freeze
 
-    # Public: The name of the feature.
+    # @return [String, Symbol] The name of the feature.
     attr_reader :name
 
-    # Public: Name converted to value safe for adapter.
+    # @return [String] Name converted to value safe for adapter.
     attr_reader :key
 
-    # Private: The adapter this feature should use.
+    # @internal The adapter this feature should use.
     attr_reader :adapter
 
-    # Private: What is being used to instrument all the things.
+    # @internal What is being used to instrument all the things.
     attr_reader :instrumenter
 
-    # Internal: Initializes a new feature instance.
+    # Initializes a new feature instance.
     #
-    # name - The Symbol or String name of the feature.
-    # adapter - The adapter that will be used to store details about this feature.
-    #
-    # options - The Hash of options.
-    #           :instrumenter - What to use to instrument all the things.
-    #
+    # @param name [Symbol,String] name of the feature.
+    # @param adapter [Adapter] The adapter that will be used to store details about this feature.
+    # @option options [#instrument] :instrumenter - What to use to instrument all the things.
+    # @internal
     def initialize(name, adapter, options = {})
       @name = name
       @key = name.to_s
@@ -36,9 +34,9 @@ module Flipper
       @adapter = adapter
     end
 
-    # Public: Enable this feature for something.
+    # Enable this feature for something.
     #
-    # Returns the result of Adapter#enable.
+    # @return the result of {Adapter#enable}
     def enable(thing = true)
       instrument(:enable) do |payload|
         adapter.add self
@@ -52,9 +50,9 @@ module Flipper
       end
     end
 
-    # Public: Disable this feature for something.
+    # Disable this feature for something.
     #
-    # Returns the result of Adapter#disable.
+    # @return the result of {Adapter#disable}
     def disable(thing = false)
       instrument(:disable) do |payload|
         adapter.add self
@@ -68,35 +66,35 @@ module Flipper
       end
     end
 
-    # Public: Adds this feature.
+    # Adds this feature.
     #
-    # Returns the result of Adapter#add.
+    # @return the result of {Adapter#add}
     def add
       instrument(:add) { adapter.add(self) }
     end
 
-    # Public: Does this feature exist in the adapter.
+    # Does this feature exist in the adapter.
     #
-    # Returns true if exists in adapter else false.
+    # @return [Boolean] true if exists in adapter else false.
     def exist?
       instrument(:exist?) { adapter.features.include?(key) }
     end
 
-    # Public: Removes this feature.
+    # Removes this feature.
     #
-    # Returns the result of Adapter#remove.
+    # @return the result of {Adapter#remove}
     def remove
       instrument(:remove) { adapter.remove(self) }
     end
 
-    # Public: Clears all gate values for this feature.
+    # Clears all gate values for this feature.
     #
-    # Returns the result of Adapter#clear.
+    # @return the result of {Adapter#clear}
     def clear
       instrument(:clear) { adapter.clear(self) }
     end
 
-    # Public: Check if a feature is enabled for zero or more actors.
+    # Check if a feature is enabled for zero or more actors.
     #
     # Returns true if enabled, false if not.
     def enabled?(*actors)
@@ -120,47 +118,45 @@ module Flipper
       end
     end
 
-    # Public: Enables a feature for an actor.
+    # Enables a feature for an actor.
     #
-    # actor - a Flipper::Types::Actor instance or an object that responds
-    #         to flipper_id.
-    #
-    # Returns result of enable.
+    # @param actor [Types::Actor, #flipper_id]
+    # @return the result of {#enable}
     def enable_actor(actor)
       enable Types::Actor.wrap(actor)
     end
 
-    # Public: Enables a feature for a group.
+    # Enables a feature for a group.
     #
     # group - a Flipper::Types::Group instance or a String or Symbol name of a
     #         registered group.
     #
-    # Returns result of enable.
+    # @return the result of {#enable}
     def enable_group(group)
       enable Types::Group.wrap(group)
     end
 
-    # Public: Enables a feature a percentage of time.
+    # Enables a feature a percentage of time.
     #
     # percentage - a Flipper::Types::PercentageOfTime instance or an object that
     #              responds to to_i.
     #
-    # Returns result of enable.
+    # @return the result of {#enable}
     def enable_percentage_of_time(percentage)
       enable Types::PercentageOfTime.wrap(percentage)
     end
 
-    # Public: Enables a feature for a percentage of actors.
+    # Enables a feature for a percentage of actors.
     #
     # percentage - a Flipper::Types::PercentageOfTime instance or an object that
     #              responds to to_i.
     #
-    # Returns result of enable.
+    # @return the result of {#enable}
     def enable_percentage_of_actors(percentage)
       enable Types::PercentageOfActors.wrap(percentage)
     end
 
-    # Public: Disables a feature for an actor.
+    # Disables a feature for an actor.
     #
     # actor - a Flipper::Types::Actor instance or an object that responds
     #         to flipper_id.
@@ -170,7 +166,7 @@ module Flipper
       disable Types::Actor.wrap(actor)
     end
 
-    # Public: Disables a feature for a group.
+    # Disables a feature for a group.
     #
     # group - a Flipper::Types::Group instance or a String or Symbol name of a
     #         registered group.
@@ -180,7 +176,7 @@ module Flipper
       disable Types::Group.wrap(group)
     end
 
-    # Public: Disables a feature a percentage of time.
+    # Disables a feature a percentage of time.
     #
     # percentage - a Flipper::Types::PercentageOfTime instance or an object that
     #              responds to to_i.
@@ -190,7 +186,7 @@ module Flipper
       disable Types::PercentageOfTime.new(0)
     end
 
-    # Public: Disables a feature for a percentage of actors.
+    # Disables a feature for a percentage of actors.
     #
     # percentage - a Flipper::Types::PercentageOfTime instance or an object that
     #              responds to to_i.
@@ -200,7 +196,7 @@ module Flipper
       disable Types::PercentageOfActors.new(0)
     end
 
-    # Public: Returns state for feature (:on, :off, or :conditional).
+    # Returns state for feature (:on, :off, or :conditional).
     def state
       values = gate_values
       boolean = gate(:boolean)
@@ -215,29 +211,29 @@ module Flipper
       end
     end
 
-    # Public: Is the feature fully enabled.
+    # Is the feature fully enabled.
     def on?
       state == :on
     end
 
-    # Public: Is the feature fully disabled.
+    # Is the feature fully disabled.
     def off?
       state == :off
     end
 
-    # Public: Is the feature conditionally enabled for a given actor, group,
+    # Is the feature conditionally enabled for a given actor, group,
     # percentage of actors or percentage of the time.
     def conditional?
       state == :conditional
     end
 
-    # Public: Returns the raw gate values stored by the adapter.
+    # Returns the raw gate values stored by the adapter.
     def gate_values
       adapter_values = adapter.get(self)
       GateValues.new(adapter_values)
     end
 
-    # Public: Get groups enabled for this feature.
+    # Get groups enabled for this feature.
     #
     # Returns Set of Flipper::Types::Group instances.
     def enabled_groups
@@ -245,49 +241,49 @@ module Flipper
     end
     alias_method :groups, :enabled_groups
 
-    # Public: Get groups not enabled for this feature.
+    # Get groups not enabled for this feature.
     #
     # Returns Set of Flipper::Types::Group instances.
     def disabled_groups
       Flipper.groups - enabled_groups
     end
 
-    # Public: Get the adapter value for the groups gate.
+    # Get the adapter value for the groups gate.
     #
     # Returns Set of String group names.
     def groups_value
       gate_values.groups
     end
 
-    # Public: Get the adapter value for the actors gate.
+    # Get the adapter value for the actors gate.
     #
     # Returns Set of String flipper_id's.
     def actors_value
       gate_values.actors
     end
 
-    # Public: Get the adapter value for the boolean gate.
+    # Get the adapter value for the boolean gate.
     #
     # Returns true or false.
     def boolean_value
       gate_values.boolean
     end
 
-    # Public: Get the adapter value for the percentage of actors gate.
+    # Get the adapter value for the percentage of actors gate.
     #
     # Returns Integer greater than or equal to 0 and less than or equal to 100.
     def percentage_of_actors_value
       gate_values.percentage_of_actors
     end
 
-    # Public: Get the adapter value for the percentage of time gate.
+    # Get the adapter value for the percentage of time gate.
     #
     # Returns Integer greater than or equal to 0 and less than or equal to 100.
     def percentage_of_time_value
       gate_values.percentage_of_time
     end
 
-    # Public: Get the gates that have been enabled for the feature.
+    # Get the gates that have been enabled for the feature.
     #
     # Returns an Array of Flipper::Gate instances.
     def enabled_gates
@@ -295,38 +291,38 @@ module Flipper
       gates.select { |gate| gate.enabled?(values.send(gate.key)) }
     end
 
-    # Public: Get the names of the enabled gates.
+    # Get the names of the enabled gates.
     #
     # Returns an Array of gate names.
     def enabled_gate_names
       enabled_gates.map(&:name)
     end
 
-    # Public: Get the gates that have not been enabled for the feature.
+    # Get the gates that have not been enabled for the feature.
     #
     # Returns an Array of Flipper::Gate instances.
     def disabled_gates
       gates - enabled_gates
     end
 
-    # Public: Get the names of the disabled gates.
+    # Get the names of the disabled gates.
     #
     # Returns an Array of gate names.
     def disabled_gate_names
       disabled_gates.map(&:name)
     end
 
-    # Public: Returns the string representation of the feature.
+    # Returns the string representation of the feature.
     def to_s
       name.to_s
     end
 
-    # Public: Identifier to be used in the url (a rails-ism).
+    # Identifier to be used in the url (a rails-ism).
     def to_param
       to_s
     end
 
-    # Public: Pretty string version for debugging.
+    # Pretty string version for debugging.
     def inspect
       attributes = [
         "name=#{name.inspect}",
@@ -337,7 +333,7 @@ module Flipper
       "#<#{self.class.name}:#{object_id} #{attributes.join(', ')}>"
     end
 
-    # Public: Get all the gates used to determine enabled/disabled for the feature.
+    # Get all the gates used to determine enabled/disabled for the feature.
     #
     # Returns an array of gates
     def gates
@@ -354,14 +350,14 @@ module Flipper
       }.freeze
     end
 
-    # Public: Find a gate by name.
+    # Find a gate by name.
     #
     # Returns a Flipper::Gate if found, nil if not.
     def gate(name)
       gates_hash[name.to_sym]
     end
 
-    # Public: Find the gate that protects an actor.
+    # Find the gate that protects an actor.
     #
     # actor - The object for which you would like to find a gate
     #
@@ -373,7 +369,7 @@ module Flipper
 
     private
 
-    # Private: Instrument a feature operation.
+    # @internal Instrument a feature operation.
     def instrument(operation, initial_payload = {})
       @instrumenter.instrument(InstrumentationName, initial_payload) do |payload|
         payload[:feature_name] = name
