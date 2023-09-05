b61773458dbdc00e55d305c1cceab9175f91410d
Brandon Keepers <brandon@opensoul.org>
2023-09-05 07:53:10 -0400

    Use base_class to determine flipper_id for ActiveRecord actors

--- lib/flipper/model/active_record.rb
+++ lib/flipper/model/active_record.rb
@@ -1,7 +1,18 @@
 module Flipper
   module Model
     module ActiveRecord
-      include Flipper::Identifier
+      # The id of the record when used as an actor.
+      #
+      #   class User < ActiveRecord::Base
+      #   end
+      #
+      #   user = User.first
+      #   Flipper.enable :some_feature, user
+      #   Flipper.enabled? :some_feature, user #=> true
+      #
+      def flipper_id
+        "#{self.class.base_class.name};#{id}"
+      end
 
       # Properties used to evaluate expressions
       def flipper_properties
