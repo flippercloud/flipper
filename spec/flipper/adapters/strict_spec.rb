23ee22b2e64eee3600051424a951169aa7eb6d69
Brandon Keepers <brandon@opensoul.org>
2023-09-16 21:56:16 -0400

    Spec for proc handler

--- spec/flipper/adapters/strict_spec.rb
+++ spec/flipper/adapters/strict_spec.rb
@@ -37,4 +37,26 @@ RSpec.describe Flipper::Adapters::Strict do
       end
     end
   end
+
+  context "handler: Proc" do
+    let(:unknown_features) { [] }
+    subject do
+      described_class.new(Flipper::Adapters::Memory.new, handler: ->(feature) { unknown_features << feature.key})
+    end
+
+
+    context "#get" do
+      it "raises an error for unknown feature" do
+        subject.get(feature)
+        expect(unknown_features).to eq(["unknown"])
+      end
+    end
+
+    context "#get_multi" do
+      it "raises an error for unknown feature" do
+        subject.get_multi([flipper[:foo], flipper[:bar]])
+        expect(unknown_features).to eq(["foo", "bar"])
+      end
+    end
+  end
 end
