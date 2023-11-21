c45e987b2e01100dfbe42f379a0ef12baa8d4078
John Nunemaker <nunemaker@gmail.com>
2023-11-21 10:03:28 -0500

    Add retry specs for 429 and 5xx

--- spec/flipper/cloud/telemetry/submitter_spec.rb
+++ spec/flipper/cloud/telemetry/submitter_spec.rb
@@ -1,33 +1,37 @@
+require "stringio"
 require 'flipper/cloud/configuration'
 require 'flipper/cloud/telemetry/submitter'
 
 RSpec.describe Flipper::Cloud::Telemetry::Submitter do
-  let(:config) {
+  let(:cloud_configuration) {
     Flipper::Cloud::Configuration.new({token: "asdf"})
   }
 
-  let(:subject) { described_class.new(config) }
+  let(:subject) { described_class.new(cloud_configuration) }
 
   describe "#initialize" do
-    it "works with cloud config" do
-      submitter = described_class.new(config)
-      expect(submitter.cloud_configuration).to eq(config)
+    it "works with cloud_configuration" do
+      submitter = described_class.new(cloud_configuration)
+      expect(submitter.cloud_configuration).to eq(cloud_configuration)
     end
   end
 
   describe "#call" do
-    it "does not submit blank metrics" do
-      expect(subject.call({})).to be(nil)
-    end
-
-    it "submits present metrics" do
-      enabled_metrics = {
+    let(:enabled_metrics) {
+      {
         Flipper::Cloud::Telemetry::Metric.new(:search, true, 1696793160) => 10,
         Flipper::Cloud::Telemetry::Metric.new(:search, false, 1696793161) => 15,
         Flipper::Cloud::Telemetry::Metric.new(:plausible, true, 1696793162) => 25,
         Flipper::Cloud::Telemetry::Metric.new(:administrator, true, 1696793164) => 1,
         Flipper::Cloud::Telemetry::Metric.new(:administrator, false, 1696793164) => 24,
       }
+    }
+
+    it "does not submit blank metrics" do
+      expect(subject.call({})).to be(nil)
+    end
+
+    it "submits present metrics" do
       expected_body = {
         "request_id" => subject.request_id,
         "enabled_metrics" =>[
@@ -53,14 +57,44 @@ RSpec.describe Flipper::Cloud::Telemetry::Submitter do
         'Schema-Version' => 'V1',
         'User-Agent' => "Flipper HTTP Adapter v#{Flipper::VERSION}",
       }
+      stub_request(:post, "https://www.flippercloud.io/adapter/telemetry").
+        with { |request|
+          gunzipped = Flipper::Typecast.from_gzip(request.body)
+          body = Flipper::Typecast.from_json(gunzipped)
+          body == expected_body && request.headers == expected_headers
+        }.to_return(status: 200, body: "{}", headers: {})
+      subject.call(enabled_metrics)
+    end
+
+    it "retries on 429" do
+      output = StringIO.new
+      cloud_configuration.telemetry_logger = Logger.new(output)
       with_env "FLIPPER_CLOUD_TELEMETRY_LOGGING" => "true" do
         stub_request(:post, "https://www.flippercloud.io/adapter/telemetry").
-          with { |request|
-            gunzipped = Flipper::Typecast.from_gzip(request.body)
-            body = Flipper::Typecast.from_json(gunzipped)
-            body == expected_body && request.headers == expected_headers
-          }.to_return(status: 200, body: "{}", headers: {})
+          to_return(status: 429, body: "{}", headers: {}).
+          to_return(status: 429, body: "{}", headers: {}).
+          to_return(status: 200, body: "{}", headers: {})
         subject.call(enabled_metrics)
+        expect(output.string).to include("retrying=true retries_remaining=10")
+        expect(output.string).to include("retrying=true retries_remaining=9")
+        expect(output.string).not_to include("retrying=true retries_remaining=8")
+      end
+    end
+
+    it "retries on 500" do
+      output = StringIO.new
+      cloud_configuration.telemetry_logger = Logger.new(output)
+      with_env "FLIPPER_CLOUD_TELEMETRY_LOGGING" => "true" do
+        stub_request(:post, "https://www.flippercloud.io/adapter/telemetry").
+          to_return(status: 500, body: "{}", headers: {}).
+          to_return(status: 503, body: "{}", headers: {}).
+          to_return(status: 502, body: "{}", headers: {}).
+          to_return(status: 200, body: "{}", headers: {})
+        subject.call(enabled_metrics)
+        expect(output.string).to include("retrying=true retries_remaining=10")
+        expect(output.string).to include("retrying=true retries_remaining=9")
+        expect(output.string).to include("retrying=true retries_remaining=8")
+        expect(output.string).not_to include("retrying=true retries_remaining=7")
       end
     end
   end
