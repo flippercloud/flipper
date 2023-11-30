5608488eef633a228b9c8f60f721f7d8f627ea35
Brandon Keepers <brandon@opensoul.org>
2023-11-30 08:19:03 -0500

    Fix kwargs for Ruby 2.6 and 2.7

--- lib/flipper/adapter_builder.rb
+++ lib/flipper/adapter_builder.rb
@@ -17,16 +17,23 @@ module Flipper
       block.arity == 0 ? instance_eval(&block) : block.call(self) if block
     end
 
-    def use(klass, *args, **kwargs, &block)
-      @stack.push ->(adapter) { klass.new(adapter, *args, **kwargs, &block) }
+    def use(klass, *args, &block)
+      @stack.push ->(adapter) { klass.new(adapter, *args, &block) }
     end
 
-    def store(adapter, *args, **kwargs, &block)
-      @store = adapter.respond_to?(:call) ? adapter : -> { adapter.new(*args, **kwargs, &block) }
+    def store(adapter, *args, &block)
+      @store = adapter.respond_to?(:call) ? adapter : -> { adapter.new(*args, &block) }
     end
 
     def to_adapter
       @stack.reverse.inject(@store.call) { |adapter, wrapper| wrapper.call(adapter) }
+    end
+
+    # Properly pass kwargs to `use` and `store` methods.
+    # Replace with `...` once support for Ruby 2.6 and 2.7 are dropped
+    if respond_to?(:ruby2_keywords, true)
+      ruby2_keywords :use
+      ruby2_keywords :store
     end
   end
 end
