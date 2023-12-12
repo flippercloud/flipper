96174f124e7b78c0e265f79a07d566bfc909b39e
Brandon Keepers <brandon@opensoul.org>
2023-12-12 12:58:15 -0500

    Show deprecation warning for Ruby < 3.0

--- lib/flipper/version.rb
+++ lib/flipper/version.rb
@@ -1,3 +1,11 @@
 module Flipper
   VERSION = '1.1.2'.freeze
+
+  REQUIRED_RUBY_VERSION = '2.6'.freeze
+
+  NEXT_REQUIRED_RUBY_VERSION = '3.0'.freeze
+
+  def self.deprecated_ruby_version?
+    Gem::Version.new(RUBY_VERSION) < Gem::Version.new(NEXT_REQUIRED_RUBY_VERSION)
+  end
 end
