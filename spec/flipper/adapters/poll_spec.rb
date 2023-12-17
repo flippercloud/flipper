e9f188fef66a4e7f782cf2d00b1a509e2af9afd6
John Nunemaker <nunemaker@gmail.com>
2023-12-17 17:05:01 -0500

    Ensure that sync is not in main thread for non-empty adapters

--- spec/flipper/adapters/poll_spec.rb
+++ spec/flipper/adapters/poll_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe Flipper::Adapters::Poll do
     instance.features # call something to force sync
     expect(local_adapter.features).to eq(remote_adapter.features)
   end
+
+  it "does not sync in main thread if local adapter is not empty" do
+    # make local not empty by importing remote
+    flipper = Flipper.new(local_adapter)
+    flipper.import(remote_adapter)
+
+    # make a fake poller to verify calls
+    poller = double("Poller", last_synced_at: Concurrent::AtomicFixnum.new(0))
+    expect(poller).to receive(:start).twice
+    expect(poller).not_to receive(:sync)
+
+    # create new instance and call something to force sync
+    instance = described_class.new(poller, local_adapter)
+    instance.features # call something to force sync
+
+    expect(local_adapter.features).to eq(remote_adapter.features)
+  end
 end
