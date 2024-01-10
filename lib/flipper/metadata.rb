2fd9de7d5158e1cf6ac28fadef72f3e02ba18cdf
Brandon Keepers <brandon@opensoul.org>
2024-01-10 09:17:40 -0500

    Link directly to changelog for version

--- lib/flipper/metadata.rb
+++ lib/flipper/metadata.rb
@@ -1,9 +1,11 @@
+require_relative './version'
+
 module Flipper
   METADATA = {
     "documentation_uri" => "https://www.flippercloud.io/docs",
     "homepage_uri"      => "https://www.flippercloud.io",
     "source_code_uri"   => "https://github.com/flippercloud/flipper",
     "bug_tracker_uri"   => "https://github.com/flippercloud/flipper/issues",
-    "changelog_uri"     => "https://github.com/flippercloud/flipper/releases",
+    "changelog_uri"     => "https://github.com/flippercloud/flipper/releases/tag/v#{Flipper::VERSION}",
   }.freeze
 end
