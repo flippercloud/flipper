573c89bd4cba8b49caeda5bfbd7ed065c576efdf
Brandon Keepers <brandon@opensoul.org>
2024-02-15 09:07:17 -0500

    Increase timeout to 60 for integration test

--- test_rails/system/test_help_test.rb
+++ test_rails/system/test_help_test.rb
@@ -29,7 +29,7 @@ end
 
 class TestHelpTest < ActionDispatch::SystemTestCase
   # Any driver that runs the app in a separate thread will test what we want here.
-  driven_by :cuprite, options: { process_timeout: 30 }
+  driven_by :cuprite, options: { process_timeout: 60 }
 
   setup do
     # Reconfigure Flipper since other tests change the adapter.
