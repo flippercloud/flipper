2efff38c8019ac7d58787878806b54b88c112209
John Nunemaker <nunemaker@gmail.com>
2024-02-15 14:09:18 -0500

    Minor improvement switching to real classes
    Removes delegation and respond to for many features/gates.
    
    ```
    Before
    Calculating -------------------------------------
        Typecast.to_json      1.567k (± 1.0%) i/s -      7.850k in   5.008514s
    
    After
    Calculating -------------------------------------
        Typecast.to_json      2.493k (± 1.0%) i/s -     12.699k in   5.094881s
    ```

--- lib/flipper/api/v1/decorators/feature.rb
+++ lib/flipper/api/v1/decorators/feature.rb
@@ -5,20 +5,21 @@ module Flipper
   module Api
     module V1
       module Decorators
-        class Feature < SimpleDelegator
-          # Public: The feature being decorated.
-          alias_method :feature, :__getobj__
+        class Feature
+          def initialize(feature)
+            @feature = feature
+          end
 
           # Public: Returns instance as hash that is ready to be json dumped.
           def as_json(exclude_gates: false, exclude_gate_names: false)
             result = {
-              'key' => key,
-              'state' => state.to_s,
+              'key' => @feature.key,
+              'state' => @feature.state.to_s,
             }
 
             unless exclude_gates
-              gate_values = feature.adapter.get(self)
-              result['gates'] = gates.map do |gate|
+              gate_values = @feature.adapter.get(@feature)
+              result['gates'] = @feature.gates.map do |gate|
                 Decorators::Gate.new(gate, gate_values[gate.key]).as_json(exclude_name: exclude_gate_names)
               end
             end
