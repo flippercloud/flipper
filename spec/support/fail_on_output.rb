028a9bb9a99df8ef01b0bac24edb9298bcec8d9e
John Nunemaker <nunemaker@gmail.com>
2024-08-27 08:37:40 -0400

    Update fail_on_output to use capture_output when checking

--- spec/support/fail_on_output.rb
+++ spec/support/fail_on_output.rb
@@ -1,7 +1,7 @@
 if ENV["CI"] || ENV["FAIL_ON_OUTPUT"]
   RSpec.configure do |config|
     config.around do |example|
-      output = silence { example.run }
+      output = capture_output { example.run }
       fail "Use `silence { }` to avoid printing to STDOUT/STDERR\n#{output}" unless output.empty?
     end
   end
